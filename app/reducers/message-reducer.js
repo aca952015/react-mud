d330f650077226e2214ea0695a1fa37656111e99
Steven Bateman <batemanvo@gmail.com>
2017-05-24 11:50:35 -0700

    Rolled back to previous commit.

--- app/reducers/message-reducer.js
+++ app/reducers/message-reducer.js
@@ -5,5 +5,6 @@ const initialState = {
 };
 
 export default function reducer(state=initialState, action) {
+  if (action.type === 'NEW_MESSAGE') return {...state, messages: [...state.messages, action.payload]};
   return state;
 }
