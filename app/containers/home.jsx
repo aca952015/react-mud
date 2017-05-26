505ffddaae693b6927d5089e55edb6cf79882a6e
Steven Bateman <batemanvo@gmail.com>
2017-05-26 09:28:02 -0700

    Refactored socket handling into its own file.

--- app/containers/home.jsx
+++ app/containers/home.jsx
@@ -5,11 +5,8 @@ import {connect} from 'react-redux';
 import PropTypes from 'prop-types';
 import io from 'socket.io-client';
 import {Messages} from '../components/messages.jsx';
-import {newMessage} from '../actions/message-actions.js';
+import socketHandlers from '../handlers/socket-handlers.js';
 import {commandHandler} from '../handlers/command-handler.js';
-import {whisperProcessor} from '../processors/whisper-processor.js';
-import {sayProcessor} from '../processors/say-processor.js';
-import {moveProcessor} from '../processors/move-processor.js';
 
 @connect(store => {
   return {
@@ -20,15 +17,7 @@ import {moveProcessor} from '../processors/move-processor.js';
 export default class Home extends Component {
   componentDidMount() {
     this.socket = io('/');
-    this.socket.username = this.props.username;
-    this.socket.currentRoom = 'Nexus';
-    this.socket.emit('changeName', this.socket.username);
-    this.socket.on('message', result => this.props.dispatch(newMessage(sayProcessor(result, this.socket))));
-    this.socket.on('occupants', result => this.props.dispatch(newMessage(result)));
-    this.socket.on('whisperSuccess', result => this.props.dispatch(newMessage(whisperProcessor(result, this.socket))));
-    this.socket.on('whisperFail', () => this.props.dispatch(newMessage({text: 'I don\'t see that person here.'})));
-    this.socket.on('movementLeave', movement => this.props.dispatch(newMessage({text: `${movement.username} moves ${movement.direction}.`})));
-    this.socket.on('movementArrive', movement => this.props.dispatch(newMessage(moveProcessor(movement))));
+    socketHandlers(this.socket, this.props);
   }
   handleCommand = event => {
     if (event.keyCode === 13) {
