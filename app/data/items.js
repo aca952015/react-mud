d3a127e466d26129b88f8ae813060fe6f8b801fc
Steven Bateman <batemanvo@gmail.com>
2017-05-31 09:22:00 -0700

    Added a test item.

---
+++ app/data/items.js
@@ -0,0 +1,9 @@
+'use strict';
+
+export const itemData = [
+  {
+    short: 'a red potion',
+    long: 'a flask, with a v-shaped bottom and a thin neck, filled with a red liquid',
+    terms: ['potion', 'red', 'flask']
+  }
+];
