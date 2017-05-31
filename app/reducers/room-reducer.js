1089d6698d667ddc19bc84470e367233b6708af0
Steven Bateman <batemanvo@gmail.com>
2017-05-31 12:59:37 -0700

    Server now maintains room state while running.

--- app/reducers/room-reducer.js
+++ app/reducers/room-reducer.js
@@ -3,6 +3,6 @@
 import {roomData} from '../data/rooms.js';
 
 export default function rooms(state=roomData, action) {
-  if (action.type === 'UPDATE_ROOMS') return {...state, state: action.payload};
+  if (action.type === 'UPDATE_ROOMS') return action.payload;
   return state;
 }
