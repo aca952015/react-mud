83bc253ba7cf01c2abf94e3853612cc475129010
Steven Bateman <batemanvo@gmail.com>
2017-06-03 12:00:55 -0700

    Added comments explaining item duplication logic.

--- app/components/inventory.jsx
+++ app/components/inventory.jsx
@@ -4,8 +4,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Inventory = props => {
+  // Reference the room.jsx component for comments explaining how item duplication
+  // is handled, as it uses the same logic.
   let counts = {};
   props.inventory.forEach(item => counts[item.name] = (counts[item.name] || 0) + 1);
+
   let inventoryInfo = props.inventory.map(item => {
     let invString = '';
     if (counts[item.name] > 1) invString += `(${counts[item.name]}) `;
@@ -15,6 +18,7 @@ export const Inventory = props => {
     if (!acc.includes(ele)) acc.push(ele);
     return acc;
   }, []);
+
   let inventory = inventoryInfo.map((item, i) => <li key={i}>{item}</li>);
   if (!inventory.length) inventory = <li>Nothing</li>;
 
--- app/components/room.jsx
+++ app/components/room.jsx
@@ -8,7 +8,16 @@ export const Room = props => {
   let room = props.message.room;
   const exits = Object.keys(room.exits).map((exit, i) => <li key={i}>{room.exits[exit].locked ? '(' : null}{exit}{room.exits[exit].locked ? ')' : null}</li>);
 
+  // Create an object that has the counts for each item in the room.items array.
+  // For example: If counts['key'] does not exist, counts['key'] is equal to 0 + 1,
+  // indicating there is 1 key in the room. If counts['key'] does exist, then the value
+  // of counts['key'] becomes the current value + 1.
   room.items.forEach(item => counts[item.name] = (counts[item.name] || 0) + 1);
+
+  // Create an array of item descriptions. If there is more than one item in the room,
+  // referenced by checking our counts object, then prepend the string with parentheses
+  // and the number of items. If not, just show the item description. Then remove
+  // duplicate values using reduce.
   let roomItemInfo = room.items.map(item => {
     let itemString = '';
     if (counts[item.name] > 1) itemString += `(${counts[item.name]}) `;
