73f6f42897346f2c24170a648529d889b4f73df1
Steven Bateman <batemanvo@gmail.com>
2017-06-04 12:55:07 -0700

    Pressing the up or down arrow cycles previous commands.

--- app/containers/home.jsx
+++ app/containers/home.jsx
@@ -16,13 +16,36 @@ import commandHandler from '../handlers/command-handler.js';
   };
 })
 export default class Home extends Component {
+  componentWillMount() {
+    this.state = {input: '', prevCommands: [], index: 1};
+  }
   componentDidMount() {
     this.socket = io('/');
     socketHandlers(this.socket, this.props);
     window.addEventListener('beforeunload', () => this.socket.emit('disconnect'));
   }
+  handleChange = event => {
+    this.setState({input: event.target.value});
+  }
   handleCommand = event => {
+    if (event.keyCode === 38 || event.keyCode === 40) {
+      if (!this.state.prevCommands.length) return;
+      if (event.keyCode === 40) {
+        console.log('Hit down');
+        if (this.state.index === 1) return this.setState({input: ''});
+        this.state.index--;
+      }
+      this.setState({input: this.state.prevCommands[this.state.prevCommands.length - this.state.index]});
+      if (event.keyCode === 38) {
+        console.log('Hit up');
+        if (this.state.index === this.state.prevCommands.length) return;
+        this.state.index++;
+      }
+    }
     if (event.keyCode === 13) {
+      this.state.prevCommands.push(event.target.value);
+      if (this.state.prevCommands.length > 20) this.state.prevCommands.shift();
+      this.setState({index: 1});
       const line = event.target.value.split(' ');
       const command = line[0].toLowerCase().trim();
       const args = line.length > 1 ? line.slice(1).join(' ').trim() : null;
@@ -31,13 +54,13 @@ export default class Home extends Component {
 
       if (result.funcsToCall && result.funcsToCall.length) result.funcsToCall.forEach(func => this.props.dispatch(func(result)));
       this.socket.emit(result.emitType, result);
-      event.target.value = '';
+      this.setState({input: ''});
     }
   }
   render() {
     return <div>
       <Messages messages={this.props.messages} inventory={this.props.inventory} />
-      <input type="text" placeholder="Enter a command" onKeyUp={this.handleCommand} />
+      <input type="text" placeholder="Enter a command" value={this.state.input} onChange={this.handleChange} onKeyUp={this.handleCommand} />
     </div>;
   }
 }
