b2f3efe52dde7868ad29cb94c3719ce2660b52bf
Steven Bateman <batemanvo@gmail.com>
2017-06-04 12:16:00 -0700

    Get can now target nth item with dot notation.

--- sockets/pick-up-item.js
+++ sockets/pick-up-item.js
@@ -3,8 +3,14 @@
 export default function pickUpItem(socket, roomData) {
   socket.on('pickUpItem', itemShort => {
     itemShort = itemShort.item;
+    let index = 0;
+    if (itemShort.split('.').length > 1) {
+      index = itemShort.split('.')[0] - 1;
+      itemShort = itemShort.split('.')[1];
+    }
     let roomItems = roomData[socket.currentRoom].items;
-    let tempItem = roomItems.find(_item => _item.terms.includes(itemShort.toLowerCase()));
+    let tempItem = index > 0 ? roomItems.filter(_item => _item.terms.includes(itemShort.toLowerCase()))[index] :
+                               roomItems.find(_item => _item.terms.includes(itemShort.toLowerCase()));
     if (!tempItem) return socket.emit('message', {text: 'I don\'t see that item here.'});
     let room = {
       item: tempItem,
