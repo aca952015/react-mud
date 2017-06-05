a3076ae39f5facc8016174475db03e803577a3cb
Steven Bateman <batemanvo@gmail.com>
2017-06-05 12:46:16 -0700

    Initial stylings on the room.

--- app/components/room.jsx
+++ app/components/room.jsx
@@ -33,8 +33,8 @@ export const Room = props => {
   return <div className="room">
     <h3>{room.roomName}</h3>
     <p>{room.desc}</p>
-    {items ? <ul>{items}</ul> : null}
-    <ul>{exits}</ul>
+    {items ? <ul id="items">{items}</ul> : null}
+    <ul id="exits">[ {exits} ]</ul>
   </div>;
 };
 
