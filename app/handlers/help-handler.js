5eaf6d363ac00d3a48557f1a6d799b273478d278
Steven Bateman <batemanvo@gmail.com>
2017-06-06 13:29:46 -0700

    Fixed capitalization issue in the help data.

--- app/handlers/help-handler.js
+++ app/handlers/help-handler.js
@@ -4,6 +4,7 @@ import {helpFile} from '../data/help-files.js';
 import {newMessage} from '../actions/message-actions.js';
 
 export default function helpHandler(command, args) {
+  if (args) args = args.toLowerCase();
   let helpObj = helpFile[args];
   if (!args) helpObj = helpFile['help'];
   let text = '';
