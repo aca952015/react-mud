294d5f3ca759273b79010a56e45de1f0ac8a8ade
Steven Bateman <batemanvo@gmail.com>
2017-06-09 14:20:16 -0700

    Fixed off by one indexing issue.

--- app/handlers/examine-handler.js
+++ app/handlers/examine-handler.js
@@ -4,7 +4,7 @@ import {newMessage} from '../actions/message-actions.js';
 
 export default function examineHandler(command, args, socket, props) {
   let splitArgs = args.split('.');
-  let index = splitArgs.length > 1 ? splitArgs[0] : 0;
+  let index = splitArgs.length > 1 ? splitArgs[0] - 1 : 0;
   let foundItem = index > 0 ? props.inventory.filter(item => item.terms.includes(splitArgs[1]))[index] :
                               props.inventory.find(item => item.terms.includes(args));
   if (!foundItem) return {funcsToCall: [newMessage], text: 'You aren\'t carrying that.'};
