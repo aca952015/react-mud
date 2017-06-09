4a8d4292d94405d6849c4a890d63d0251615227d
Steven Bateman <batemanvo@gmail.com>
2017-06-09 14:09:26 -0700

    Refactored server-side look targeting logic.

--- sockets/look.js
+++ sockets/look.js
@@ -6,8 +6,6 @@ export default function look(socket, users, roomInfo) {
       socket.emit('generalMessage', {text: target.description});
     }
 
-    if (args.target) args.target = args.target.toLowerCase();
-
     let room = {
       roomName: socket.currentRoom,
       desc: roomInfo[socket.currentRoom].desc,
@@ -19,26 +17,35 @@ export default function look(socket, users, roomInfo) {
     let occupants = users.filter(user => user.username && user.currentRoom === socket.currentRoom && user.username !== socket.username)
     .map(user => user.username);
 
-    let lookTarget = room.items.find(item => item.terms.includes(args.target));
-    if (lookTarget) {
-      socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, text: ` looks at ${lookTarget.short}.`});
-      return showMeTheDescription(lookTarget);
+    if (args.target) {
+      args.target = args.target.toLowerCase();
+
+      let splitArgs = args.target.split('.');
+      let lookTarget = splitArgs.length > 1 ?
+                       room.items.filter(item => item.terms.includes(splitArgs[1]))[splitArgs[0] - 1] :
+                       room.items.find(item => item.terms.includes(args.target));
+
+      if (lookTarget) {
+        socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, text: ` looks at ${lookTarget.short}.`});
+        return showMeTheDescription(lookTarget);
+      }
+
+      lookTarget = occupants.find(player => player.toLowerCase() === args.target);
+      if (lookTarget) {
+        let player = users.find(user => user.username === lookTarget);
+        socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, lookType: ' looks at ', text: ' ', target: lookTarget});
+        return showMeTheDescription(player);
+      }
+
+      lookTarget = room.examines ? room.examines.find(examine => examine.terms.includes(args.target)) : null;
+      if (lookTarget) {
+        socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, text: ` looks at ${lookTarget.name}.`});
+        return showMeTheDescription(lookTarget);
+      }
+
+      return socket.emit('generalMessage', {text: 'I don\'t see that here.'});
     }
 
-    lookTarget = occupants.find(player => player.toLowerCase() === args.target);
-    if (lookTarget) {
-      let player = users.find(user => user.username === lookTarget);
-      socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, lookType: ' looks at ', text: ' ', target: lookTarget});
-      return showMeTheDescription(player);
-    }
-
-    lookTarget = room.examines ? room.examines.find(examine => examine.terms.includes(args.target)) : null;
-    if (lookTarget) {
-      socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, text: ` looks at ${lookTarget.name}.`});
-      return showMeTheDescription(lookTarget);
-    }
-
-    if (!args.target) return socket.emit('generalMessage', {occupants, room});
-    return socket.emit('generalMessage', {text: 'I don\'t see that here.'});
+    socket.emit('generalMessage', {occupants, room});
   });
 }
