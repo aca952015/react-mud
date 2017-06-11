dfc7cd20e499155b6bcec08a8b6a82174f90a1b3
Steven Bateman <batemanvo@gmail.com>
2017-06-11 12:55:24 -0700

    Added cleaner comments to communication and feedback.

--- app/components/communication.jsx
+++ app/components/communication.jsx
@@ -4,11 +4,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Communication = props => {
+  // If the user is whispering to themself, we do not need a comma and quotes.
+  // If the user is a third party to a whisper, they do not need a comma and quotes.
+  // If the message is a say and the user is saying, set theFrom to be 'You '.
+  // Give quotes a variable because the editor highlights strangely otherwise.
   let selfWhisper = props.message.commType === ' whispers something quietly.';
   let thirdParty = props.message.commType === ' whispers something to ';
   let theFrom = props.message.from.toLowerCase() === props.username.toLowerCase() ? 'You' : props.message.from;
   let quotes = '"';
 
+  // If there's a target, show it. Use the above variables to decide on correct grammar.
+  // If quotes are necessary, based on the above variables, insert them.
   return <p className="communication">
     <span className="source">{theFrom}</span>
     <span className="comm-type">{props.message.commType}</span>
