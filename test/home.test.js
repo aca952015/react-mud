b3d51ad3521cdfd361ace8b5d79d3d842ee8605a
Steven Bateman <batemanvo@gmail.com>
2017-06-14 15:25:27 -0700

    Added test for Home container handleChange method.

--- test/home.test.js
+++ test/home.test.js
@@ -3,38 +3,25 @@
 import React from 'react';
 import sinon from 'sinon';
 import {shallow} from 'enzyme';
+import {updateInput} from '../app/actions/message-actions.js';
 import {Home} from '../app/containers/home.jsx';
 
 describe('<Home />', () => {
-  let home, props;
-  beforeEach(done => {
-    props = {
-      dispatch: sinon.spy(),
-      messages: [],
-      commandIndex: 0,
-      username: 'Tester',
-      inventory: [],
-      character: {
-        username: 'Tester',
-        description: 'Test description',
-        inventory: [],
-        hp: 18,
-        maxHP: 20,
-        mp: 11,
-        maxMP: 20,
-        level: 1,
-        str: 18,
-        int: 18,
-        wis: 18,
-        con: 18,
-        dex: 18
-      }
-    };
-    home = shallow(<Home {...props} />);
-    done();
-  });
+  let props = {
+    dispatch: sinon.spy(),
+    messages: [],
+    commandIndex: 0,
+    username: 'Tester',
+    inventory: [],
+  };
+  let home = shallow(<Home {...props} />);
 
   it('has an initialState of justHitEnter being false', () => {
     expect(home.state().justHitEnter).toEqual(false);
+  });
+
+  it('handleChange should dispatch a correct updateInput', () => {
+    home.instance().handleChange({target: {value: 'Test value'}});
+    expect(props.dispatch.calledWith(updateInput('Test value', props.commandIndex))).toEqual(true);
   });
 });
