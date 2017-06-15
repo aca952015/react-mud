8cb49f687eb024610971399f7450e0937f6cdcf8
Steven Bateman <batemanvo@gmail.com>
2017-06-15 14:04:08 -0700

    Added a test for locked exits.

--- test/components/room.test.js
+++ test/components/room.test.js
@@ -29,4 +29,30 @@ describe('<Room />', () => {
     expect(room.find('li').text()).toEqual('up');
     expect(room.find('ul').last().text()).toEqual('Exits: [ up ]');
   });
+
+  it('should render a roomName, description, and locked exits if there are any', () => {
+    props = {
+      message: {
+        room: {
+          roomName: 'Test room',
+          desc: 'Test room desc',
+          exits: {
+            up: {
+              exit: 'Nexus',
+              locked: true
+            },
+            east: {
+              exit: 'Tester room B',
+              locked: false
+            }
+          },
+          items: []
+        }
+      }
+    };
+    room = shallow(<Room {...props} />);
+    expect(room.find('ul').last().text()).toEqual('Exits: [ (up)east ]');
+    expect(room.find('ul').last().children('li').at(0).text()).toEqual('(up)');
+    expect(room.find('ul').last().children('li').at(1).text()).toEqual('east');
+  });
 });
