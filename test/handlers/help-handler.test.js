67f86dc41951bec1cdf33a3524f9dd914bf7ecfe
Steven Bateman <batemanvo@gmail.com>
2017-06-15 15:07:49 -0700

    Added test for help handler with a valid help file.

--- test/handlers/help-handler.test.js
+++ test/handlers/help-handler.test.js
@@ -6,11 +6,22 @@ import helpHandler from '../../app/handlers/help-handler.js';
 
 describe('helpHandler', () => {
   let returnObj = {funcsToCall: [newMessage]};
+
   describe('Without args', () => {
     it('should return the helpObj with the overall help file', () => {
       expect(helpHandler('help')).toEqual({
         ...returnObj,
         helpObj: helpFile['help'],
+        feedback: ''
+      });
+    });
+  });
+
+  describe('With args and a valid help file', () => {
+    it('should return the helpObj with the appropriate file', () => {
+      expect(helpHandler('help', 'say')).toEqual({
+        ...returnObj,
+        helpObj: helpFile['say'],
         feedback: ''
       });
     });
