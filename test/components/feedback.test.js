3a55af8602031ae5ef692e9953ab8d81a8ffd0e5
Steven Bateman <batemanvo@gmail.com>
2017-06-15 13:03:37 -0700

    Added a feedback test with a from and feedback.

--- test/components/feedback.test.js
+++ test/components/feedback.test.js
@@ -16,4 +16,16 @@ describe('<Feedback />', () => {
     feedback = shallow(<Feedback {...props} />);
     expect(feedback.find('span').text()).toEqual(props.message.feedback);
   });
+
+  it('should render the from field if the feedback involves another user', () => {
+    props = {
+      message: {
+        from: 'tester',
+        feedback: ' picks up something.'
+      }
+    };
+    feedback = shallow(<Feedback {...props} />);
+    expect(feedback.find('span').at(0).text()).toEqual(props.message.from);
+    expect(feedback.find('span').at(1).text()).toEqual(props.message.feedback);
+  });
 });
