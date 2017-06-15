4add3f87177f2d0e273037fa5d86e02f7c9963e3
Steven Bateman <batemanvo@gmail.com>
2017-06-15 16:34:49 -0700

    Added a test for NEW_MESSAGE action.

---
+++ test/actions/message-actions.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+import {newMessage, updateInput, updateCommandIndex, updatePrevCommands, truncatePrevCommands} from '../../app/actions/message-actions.js';
+
+describe('message actions', () => {
+  describe('newMessage', () => {
+    it('should return a type of NEW_MESSAGE and a payload of whatever got passed in', () => {
+      expect(newMessage('Boyyyy')).toEqual({type: 'NEW_MESSAGE', payload: 'Boyyyy'});
+    });
+  });
+});
