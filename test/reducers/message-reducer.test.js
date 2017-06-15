6e46ef76fdfddef01ce87973db5748c0046eb6aa
Steven Bateman <batemanvo@gmail.com>
2017-06-15 16:44:21 -0700

    Added a test for a NEW_MESSAGE action on the reducer.

--- test/reducers/message-reducer.test.js
+++ test/reducers/message-reducer.test.js
@@ -3,12 +3,23 @@
 import reducer from '../../app/reducers/message-reducer.js';
 
 describe('message reducer', () => {
+  let initialState = {
+    messages: [],
+    input: '',
+    prevCommands: [],
+    commandIndex: 0
+  };
+
   it('should return an initialState with nothing passed in', () => {
-    expect(reducer(undefined, {})).toEqual({
-      messages: [],
-      input: '',
-      prevCommands: [],
-      commandIndex: 0
+    expect(reducer(undefined, {})).toEqual(initialState);
+  });
+
+  describe('With a NEW_MESSAGE action', () => {
+    it('should return the initial state, with the messages array updated with the payload', () => {
+      expect(reducer(undefined, {type: 'NEW_MESSAGE', payload: 'New message'})).toEqual({
+        ...initialState,
+        messages: ['New message']
+      });
     });
   });
 });
