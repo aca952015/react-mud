596295a18ac74691cccd3d1ceaed381b1e24f31a
Steven Bateman <batemanvo@gmail.com>
2017-06-15 15:03:28 -0700

    Added get test with args.

--- test/handlers/get-handler.test.js
+++ test/handlers/get-handler.test.js
@@ -9,4 +9,10 @@ describe('getHandler', () => {
       expect(getHandler('get')).toEqual({funcsToCall: [newMessage], feedback: 'Get what?'});
     });
   });
+
+  describe('With args', () => {
+    it('should return a proper emit object to be handled by the server', () => {
+      expect(getHandler('get', 'potion')).toEqual({emitType: 'pickUpItem', item: 'potion'});
+    });
+  });
 });
