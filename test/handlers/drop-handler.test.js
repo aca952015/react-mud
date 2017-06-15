c03573416200893091f3a95af3529ce83bf88d6f
Steven Bateman <batemanvo@gmail.com>
2017-06-15 14:48:07 -0700

    Added a drop test with dot notation targeting.

--- test/handlers/drop-handler.test.js
+++ test/handlers/drop-handler.test.js
@@ -2,14 +2,29 @@
 
 import {newMessage} from '../../app/actions/message-actions.js';
 import {dropItem} from '../../app/actions/inventory-actions.js';
+import {itemData} from '../../app/data/items.js';
 import dropHandler from '../../app/handlers/drop-handler.js';
 
 describe('dropHandler', () => {
   let returnObj = {funcsToCall: [newMessage]};
+  let props = {
+    inventory: [itemData['health potion'], itemData['mana potion'], itemData['health potion']]
+  };
 
   describe('With no args', () => {
     it('should return an error object with the feedback "Drop what?"', () => {
       expect(dropHandler('drop')).toEqual({...returnObj, feedback: 'Drop what?'});
+    });
+  });
+
+  describe('With dot notation targeting', () => {
+    it('should return a drop object with the right item', () => {
+      expect(dropHandler('drop', '3.potion', null, props)).toEqual({
+        emitType: 'drop',
+        item: itemData['health potion'],
+        funcsToCall: [newMessage, dropItem],
+        feedback: `You drop ${itemData['health potion'].short}.`
+      });
     });
   });
 });
