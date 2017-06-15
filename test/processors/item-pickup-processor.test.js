81321212e89d2120ef105ef787405d43f51896e6
Steven Bateman <batemanvo@gmail.com>
2017-06-15 15:27:30 -0700

    Added a test for pickup processor when the user is in the same room.

--- test/processors/item-pickup-processor.test.js
+++ test/processors/item-pickup-processor.test.js
@@ -8,12 +8,22 @@ describe('itemPickUpProcessor', () => {
     room: {
       from: 'TestR',
       pickRoom: 'Nexus',
-      roomItems: [itemData['health potion'], itemData['gallows key']]
+      roomItems: [itemData['health potion'], itemData['gallows key']],
+      item: itemData['health potion']
     }
   };
   describe('The user is not in the same room', () => {
     it('should return an empty object', () => {
       expect(itemPickUpProcessor(room, {currentRoom: 'Gallows'})).toEqual({});
+    });
+  });
+
+  describe('The user is in the same room', () => {
+    it('should return a pick object to be processed by the newMessage action', () => {
+      expect(itemPickUpProcessor(room, {currentRoom: 'Nexus'})).toEqual({
+        from: room.from,
+        feedback: ` picks up ${room.room.item.short}.`
+      });
     });
   });
 });
