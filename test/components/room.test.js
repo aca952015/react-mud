62ed37217310ab1253c55d9f93072e747bc27984
Steven Bateman <batemanvo@gmail.com>
2017-06-15 14:06:41 -0700

    Added a test for items existing in the room.

--- test/components/room.test.js
+++ test/components/room.test.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 import {Room} from '../../app/components/room.jsx';
+import {itemData} from '../../app/data/items.js';
 
 describe('<Room />', () => {
   let props, room;
@@ -30,7 +31,7 @@ describe('<Room />', () => {
     expect(room.find('ul').last().text()).toEqual('Exits: [ up ]');
   });
 
-  it('should render a roomName, description, and locked exits if there are any', () => {
+  it('should render locked exits if there are any', () => {
     props = {
       message: {
         room: {
@@ -54,5 +55,26 @@ describe('<Room />', () => {
     expect(room.find('ul').last().text()).toEqual('Exits: [ (up)east ]');
     expect(room.find('ul').last().children('li').at(0).text()).toEqual('(up)');
     expect(room.find('ul').last().children('li').at(1).text()).toEqual('east');
+  });
+
+  it('should render items if there are any', () => {
+    props = {
+      message: {
+        room: {
+          roomName: 'Test room',
+          desc: 'Test room desc',
+          exits: {
+            up: {
+              exit: 'Blah',
+              locked: false
+            }
+          },
+          items: [itemData['health potion'], itemData['mana potion']]
+        }
+      }
+    };
+    room = shallow(<Room {...props} />);
+    expect(room.find('ul').first().children('li').at(0).text()).toEqual(props.message.room.items[0].long);
+    expect(room.find('ul').first().children('li').at(1).text()).toEqual(props.message.room.items[1].long);
   });
 });
