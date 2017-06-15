9a672320ae1f16007870f85c132c45d4ea746266
Steven Bateman <batemanvo@gmail.com>
2017-06-15 14:23:54 -0700

    Added a communication test for say with args.

--- test/handlers/communication-handler.test.js
+++ test/handlers/communication-handler.test.js
@@ -4,10 +4,22 @@ import communicationHandler from '../../app/handlers/communication-handler.js';
 import {newMessage} from '../../app/actions/message-actions.js';
 
 describe('communicationHandler', () => {
+  let props = {username: 'tester'};
   let returnObj = {funcsToCall: [newMessage]};
+
   describe('Say', () => {
     it('should return an error object if no args are given', () => {
       expect(communicationHandler('say')).toEqual({...returnObj, feedback: 'Say what?'});
+    });
+
+    it('should return a say object with args', () => {
+      expect(communicationHandler('say', 'hello', null, props)).toEqual({
+        ...returnObj,
+        from: props.username,
+        text: 'hello',
+        emitType: 'say',
+        commType: ' say, '
+      });
     });
   });
 });
