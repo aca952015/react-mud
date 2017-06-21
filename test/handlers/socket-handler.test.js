e16e54864c8f3cb2d2bcfc901adc755e2934bdca
Steven Bateman <batemanvo@gmail.com>
2017-06-21 12:27:31 -0700

    Added a test for whisperSuccess.

--- test/handlers/socket-handler.test.js
+++ test/handlers/socket-handler.test.js
@@ -12,32 +12,42 @@ import itemPickUpProcessor from '../../app/processors/item-pickup-processor.js';
 import {getItem} from '../../app/actions/inventory-actions.js';
 
 describe('socketHandlers', () => {
-  let player, url = 'http://0.0.0.0:5000';
+  let player1, player2, url = 'http://0.0.0.0:5000';
+  require('../lib/test-server.js');
   let props = {
+    username: 'player1',
     dispatch: sinon.spy(),
     character: {
       description: 'Test description'
     }
   };
-  beforeAll(done => {
-    player = io.connect(url, ioOptions);
-    player.on('connect', () => {
-      socketHandlers(player, props);
+  beforeEach(done => {
+    player1 = io.connect(url, ioOptions);
+    player2 = io.connect(url, ioOptions);
+    player2.on('connect', () => {
+      player1.emit('changeName', 'player1');
+      player2.emit('changeName', 'player2');
+      socketHandlers(player1, props);
       done();
     });
   });
 
+  afterEach(done => {
+    player1.disconnect();
+    player2.disconnect();
+    done();
+  });
+
   afterAll(done => {
-    player.disconnect();
     closeServer();
     done();
   });
 
   describe('Move', () => {
     it('should change the player\'s currentRoom to whatever got passed in', done => {
-      player.emit('move', {direction: 'down'});
-      player.on('move', res => {
-        expect(player.currentRoom).toEqual(res);
+      player1.emit('move', {direction: 'down'});
+      player1.on('move', res => {
+        expect(player1.currentRoom).toEqual(res);
         done();
       });
     });
@@ -45,9 +55,19 @@ describe('socketHandlers', () => {
 
   describe('generalMessage', () => {
     it('should dispatch a newMessage with the result', done => {
-      player.emit('lock', {direction: 'down'});
-      player.on('generalMessage', res => {
+      player1.emit('lock', {direction: 'down'});
+      player1.on('generalMessage', res => {
         expect(props.dispatch.calledWith(newMessage(res))).toEqual(true);
+        done();
+      });
+    });
+  });
+
+  describe('whisperSuccess', () => {
+    it('should dispatch a newMessage with the result passed into the whisperProcessor', done => {
+      player1.emit('whisper', {target: 'player2'});
+      player1.on('whisperSuccess', res => {
+        expect(props.dispatch.calledWith(newMessage(whisperProcessor(res, player1)))).toEqual(true);
         done();
       });
     });
