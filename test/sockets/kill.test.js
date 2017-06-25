aaa911cfa888de497b469746e7a65907e6a023a9
Steven Bateman <batemanvo@gmail.com>
2017-06-25 13:17:34 -0700

    Added a test for kill on a valid target with dot notation.

--- test/sockets/kill.test.js
+++ test/sockets/kill.test.js
@@ -3,20 +3,24 @@
 import io from 'socket.io-client';
 import closeServer from '../lib/test-server.js';
 import ioOptions from '../lib/io-options.js';
+import newMob from '../../app/data/mobs.js';
 
 describe('Kill', () => {
-  let player1;
+  let player1, player2;
 
   beforeEach(done => {
     player1 = io.connect('http://0.0.0.0:5000', ioOptions);
+    player2 = io.connect('http://0.0.0.0:5000', ioOptions);
     player1.on('connect', () => {
       player1.emit('changeName', 'player1');
+      player2.emit('changeName', 'player2');
       done();
     });
   });
 
   afterEach(done => {
     player1.disconnect();
+    player2.disconnect();
     done();
   });
 
@@ -31,6 +35,19 @@ describe('Kill', () => {
       player1.on('generalMessage', res => {
         expect(res.feedback).toEqual('Kill what?');
         done();
+      });
+    });
+  });
+
+  describe('With dot notation', () => {
+    describe('On an enemy that exists', () => {
+      it('should return an enterCombat event', done => {
+        player1.emit('kill', {target: '2.bat'});
+        player1.on('enterCombat', res => {
+          let bat = newMob('bat');
+          expect(res).toEqual({...bat, id: res.id});
+          done();
+        });
       });
     });
   });
