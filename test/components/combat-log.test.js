bf2fb2a54cfa2a557da27f265d4eed34ae1f0101
Steven Bateman <batemanvo@gmail.com>
2017-06-26 13:11:33 -0700

    Added a combatLog test for basic interactions target -> user.

--- test/components/combat-log.test.js
+++ test/components/combat-log.test.js
@@ -8,28 +8,50 @@ describe('<CombatLog />', () => {
   let props, combatLog;
 
   describe('With a basic interaction from a user against a target', () => {
-    props = {
-      message: {
-        combatLog: {
-          from: {
-            friendly: 'You'
-          },
-          interaction: ' do some stuff to ',
-          target: {
-            enemy: 'test target',
-          },
-          punctuation: '.'
-        }
-      }
-    };
-    combatLog = shallow(<CombatLog {...props} />);
-
     it('should contain the text "You do some stuff to test target."', () => {
+      props = {
+        message: {
+          combatLog: {
+            from: {
+              friendly: 'You'
+            },
+            interaction: ' do some stuff to ',
+            target: {
+              enemy: 'test target',
+            },
+            punctuation: '.'
+          }
+        }
+      };
+      combatLog = shallow(<CombatLog {...props} />);
+
       expect(combatLog.find('p').text()).toEqual('You do some stuff to test target.');
       expect(combatLog.find('.from-friendly').text()).toEqual('You');
       expect(combatLog.find('span').at(1).text()).toEqual(' do some stuff to ');
       expect(combatLog.find('.target-enemy').text()).toEqual('test target');
       expect(combatLog.find('span').last().text()).toEqual('.');
+    });
+  });
+
+  describe('With a basic interaction from a target against a user', () => {
+    it('should contain the text "Some dude targets you."', () => {
+      props = {
+        message: {
+          combatLog: {
+            from: {
+              enemy: 'Some dude'
+            },
+            interaction: ' targets ',
+            target: {
+              friendly: 'you'
+            },
+            punctuation: '.'
+          }
+        }
+      };
+      combatLog = shallow(<CombatLog {...props} />);
+      
+      expect(combatLog.find('p').text()).toEqual('Some dude targets you.');
     });
   });
 });
