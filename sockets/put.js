271d7e6cb95f24f19151712961f4dcf1fdf942a8
Steven Bateman <batemanvo@gmail.com>
2017-06-27 13:57:57 -0700

    Added put logic to items in the room.

--- sockets/put.js
+++ sockets/put.js
@@ -1,5 +1,23 @@
 'use strict';
 
 export default function put(socket, roomData) {
-  
+  /*
+  emitType: 'put',
+  item: putItem,
+  container: splitArgs[1]
+  */
+  socket.on('put', putObj => {
+    let splitArgs = putObj.container.split('.');
+    let container = splitArgs.length > 1 ? roomData[socket.currentRoom].items.filter(item => item.terms.includes(splitArgs[1]))[splitArgs[0] - 1] :
+                                           roomData[socket.currentRoom].items.find(item => item.terms.includes(putObj.container));
+    if (!container) return socket.emit('generalMessage', {feedback: 'I don\'t see that container here.'});
+    if (!container.container) return socket.emit('generalMessage', {feedback: 'That isn\'t a container.'});
+    if (!container.container.holds.includes(putObj.item.type)) return socket.emit('generalMessage', {feedback: 'That container doesn\'t hold that type of item.'});
+
+    socket.emit('forceDrop', putObj.item);
+    socket.emit('generalMessage', {feedback: `You put ${putObj.item.short} in ${container.short}.`});
+    socket.broadcast.to(socket.currentRoom).emit('generalMessage', {feedback: `${socket.username} puts ${putObj.item.short} in ${container.short}.`});
+
+    container.container.contains.push(putObj.item);
+  });
 }
