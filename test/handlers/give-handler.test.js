40b1b12043aca7f33b69e90073cd9d8d307d10bd
Steven Bateman <batemanvo@gmail.com>
2017-06-27 11:32:37 -0700

    Added a test for normal targeting but not carrying the item.

--- test/handlers/give-handler.test.js
+++ test/handlers/give-handler.test.js
@@ -5,12 +5,25 @@ import newItem from '../../app/data/items.js';
 import {newMessage} from '../../app/actions/message-actions.js';
 
 describe('giveHandler', () => {
+  let props = {inventory: [newItem('health potion')]};
+
   let defaultObj = {funcsToCall: [newMessage]};
   describe('Without the correct number of arguments', () => {
     it('should return a feedback error of "give what to whom"', () => {
-      expect(giveHandler('give', 'potion', null, {inventory: newItem('health potion')})).toEqual({
+      expect(giveHandler('give', 'potion', null, props)).toEqual({
         ...defaultObj,
         feedback: 'Give what to whom? (format: GIVE <item> <target>)'
+      });
+    });
+  });
+
+  describe('An item the user isn\'t carrying', () => {
+    describe('Without dot notation', () => {
+      it('should return a feedback error of not seeing that item', () => {
+        expect(giveHandler('give', 'bob bob', null, props)).toEqual({
+          ...defaultObj,
+          feedback: 'You don\'t seem to be carrying that.'
+        });
       });
     });
   });
