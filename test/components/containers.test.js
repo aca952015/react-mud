10ccc0c64d25758562f47428616c72dcb4cf3b80
Steven Bateman <batemanvo@gmail.com>
2017-06-27 18:58:58 -0700

    Added tests for the lis of a containers component.

--- test/components/containers.test.js
+++ test/components/containers.test.js
@@ -17,4 +17,20 @@ describe('<Containers />', () => {
   it('should render an h3 that says "You look inside and see:"', () => {
     expect(containers.find('h3').text()).toEqual('You look inside and see:');
   });
+
+  it('should render a ul with an li for each item', () => {
+    expect(containers.find('li').first().text()).toEqual('a red potion');
+    expect(containers.find('li').last().text()).toEqual('a small black key');
+  });
+
+  it('should render a ul with an li that says "nothing" if there is nothing', () => {
+    props = {
+      message: {
+        containedItems: []
+      }
+    };
+    containers = shallow(<Containers {...props} />);
+
+    expect(containers.find('li').text()).toEqual('Nothing');
+  });
 });
