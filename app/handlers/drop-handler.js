7f6487c26dbf124ede0ef9e6771c264f3e916421
Steven Bateman <batemanvo@gmail.com>
2017-06-29 11:35:28 -0700

    Fixed case sensitivity for drop.

--- app/handlers/drop-handler.js
+++ app/handlers/drop-handler.js
@@ -5,6 +5,7 @@ import {dropItem} from '../actions/inventory-actions.js';
 
 export default function dropHandler(command, args, socket, props) {
   if (!args) return {funcsToCall: [newMessage], feedback: 'Drop what?'};
+  args = args.toLowerCase();
   let index = 0;
   if (args.split('.').length > 1) index = args.split('.')[0] - 1;
   let droppedItem = index > 0 ? props.inventory.filter(item => item.terms.includes(args.split('.')[1].toLowerCase()))[index] :
