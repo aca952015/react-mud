0a7e3add92e7e322057e270e766f8e967deb469e
Steven Bateman <batemanvo@gmail.com>
2017-07-03 13:18:43 -0700

    Refactored equipment component and test for two-column layout.

--- app/components/equipment.jsx
+++ app/components/equipment.jsx
@@ -5,14 +5,12 @@ import PropTypes from 'prop-types';
 
 export const Equipment = props => {
   const equips = props.equipment;
-  const eq = Object.keys(equips).map((slot, i) => {
-    return <li key={i}>
-      {`<${slot[0].toUpperCase()}${slot.slice(1)}>`} {equips[slot] ? <span>{equips[slot].short}</span> : <span>Nothing</span>}
-    </li>;
-  });
+  const slots = Object.keys(equips).map((slot, i) => <li key={i}>{`<${slot[0].toUpperCase()}${slot.slice(1)}>`}</li>);
+  const items = Object.keys(equips).map((slot, i) => <li key={i}>{equips[slot] ? equips[slot].short : 'Nothing'}</li>);
   return <div className="equipment">
     <h3>{props.name ? `${props.name} is ` : 'You are '}wearing:</h3>
-    <ul>{eq}</ul>
+    <ul>{slots}</ul>
+    <ul>{items}</ul>
   </div>;
 };
 
