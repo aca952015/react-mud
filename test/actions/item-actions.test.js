38df97f618ddffdc09ebc6119b3a6421d7552bb4
Steven Bateman <batemanvo@gmail.com>
2017-07-03 10:50:45 -0700

    Added a test for new REMOVE_ITEM action.

--- test/actions/item-actions.test.js
+++ test/actions/item-actions.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import {restoreStat, wearEquipment} from '../../app/actions/item-actions.js';
+import {restoreStat, wearEquipment, removeItem} from '../../app/actions/item-actions.js';
 
 describe('item actions', () => {
   describe('restoreStat', () => {
@@ -20,8 +20,14 @@ describe('item actions', () => {
   });
 
   describe('wearEquipment', () => {
-    it('should return an object with type "WEAR_EQUIPMENT" and payload of whatever was passed in', () => {
+    it('should return an object with type "WEAR_EQUIPMENT" and payload of the equip property of whatever was passed in', () => {
       expect(wearEquipment({equip: 'something'})).toEqual({type: 'WEAR_EQUIPMENT', payload: 'something'});
+    });
+  });
+
+  describe('removeItem', () => {
+    it('should return an object with type "REMOVE_ITEM" and payload of whatever was passed in', () => {
+      expect(removeItem({equip: 'something'})).toEqual({type: 'REMOVE_ITEM', payload: 'something'});
     });
   });
 });
