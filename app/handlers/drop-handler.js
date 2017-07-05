a29c111d205f8e0eef1bd4a5405b32a427874dfb
Steven Bateman <batemanvo@gmail.com>
2017-07-05 13:18:43 -0700

    Added drop all to the dropHandler.

--- app/handlers/drop-handler.js
+++ app/handlers/drop-handler.js
@@ -1,12 +1,22 @@
 'use strict';
 
 import {newMessage} from '../actions/message-actions.js';
-import {dropItem} from '../actions/inventory-actions.js';
+import {dropItem, dropAll} from '../actions/inventory-actions.js';
 import termsProcessor from '../processors/terms-processor.js';
 
 export default function dropHandler(command, args, props) {
   if (!args) return {funcsToCall: [newMessage], feedback: 'Drop what?'};
   args = args.toLowerCase();
+
+  if (args === 'all') {
+    if (!props.inventory.length) return {funcsToCall: [newMessage], feedback: 'You aren\'t carrying anything to drop.'};
+    return {
+      emitType: 'dropAll',
+      itemArray: props.inventory,
+      funcsToCall: [newMessage, dropAll],
+      feedback: 'You drop everything you\'re carrying.'
+    };
+  }
 
   let droppedItem = termsProcessor(props.inventory, args.split('.'));
   if (!droppedItem) return {funcsToCall: [newMessage], feedback: 'You don\'t seem to be carrying that.'};
