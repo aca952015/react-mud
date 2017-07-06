860dafa46993257024ebc3a514816f336d1f4178
Steven Bateman <batemanvo@gmail.com>
2017-07-06 12:39:43 -0700

    Reviewed reducers and added comments. Fixed a few mutation issues in ADD_TO_CONTAINER and GET_FROM_CONTAINER.

--- app/reducers/equipment-reducer.js
+++ app/reducers/equipment-reducer.js
@@ -10,6 +10,8 @@ export const initialState = {
 
 export default function reducer(state=initialState, action) {
   if (action.type === 'WEAR_EQUIPMENT') {
+    // Since we can't use rest spread operators to directly reference a variable
+    // property, we create a copy and reference it that way.
     let newState = {...state};
     newState[action.payload.slot] = action.payload;
     return newState;
