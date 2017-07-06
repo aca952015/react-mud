1c770e9fd9c77666c807e66ff5cf7d4465ee72cc
Steven Bateman <batemanvo@gmail.com>
2017-07-06 10:51:00 -0700

    Added give all functionality to handler and socket. Also refactored give item to not call dropItem until the target is found by the server.

--- app/handlers/give-handler.js
+++ app/handlers/give-handler.js
@@ -1,7 +1,6 @@
 'use strict';
 
 import {newMessage} from '../actions/message-actions.js';
-import {dropItem} from '../actions/inventory-actions.js';
 import termsProcessor from '../processors/terms-processor.js';
 
 export default function giveHandler(command, args, props) {
@@ -9,13 +8,22 @@ export default function giveHandler(command, args, props) {
   let splitArgs = args.split(' ');
   if (!args || splitArgs.length < 2) return {funcsToCall: [newMessage], feedback: 'Give what to whom? (format: GIVE <item> <target>)'};
   if (splitArgs[1].toLowerCase() === props.username.toLowerCase()) return {funcsToCall: [newMessage], feedback: 'You can\'t give items to yourself.'};
+
+  if (splitArgs[0] === 'all') {
+    if (!props.inventory.length) return {funcsToCall: [newMessage], feedback: 'You aren\'t carrying anything to give.'};
+    
+    return {
+      emitType: 'giveAll',
+      itemArray: props.inventory,
+      target: splitArgs[1]
+    };
+  }
   let dotNotation = splitArgs[0].split('.');
 
   let item = termsProcessor(props.inventory, dotNotation);
   if (!item) return {funcsToCall: [newMessage], feedback: 'You don\'t seem to be carrying that.'};
 
   return {
-    funcsToCall: [dropItem],
     emitType: 'give',
     item,
     target: splitArgs[1]
--- sockets/give.js
+++ sockets/give.js
@@ -6,6 +6,7 @@ export default function give(socket, users) {
     if (!target || target.currentRoom !== socket.currentRoom) return socket.emit('generalMessage', {feedback: 'I don\'t see that person here.'});
 
     target.emit('forceGet', giveObj.item);
+    socket.emit('forceDrop', giveObj.item);
     socket.emit('generalMessage', {
       from: 'You',
       interaction: ` give ${giveObj.item.short} to `,
@@ -14,6 +15,20 @@ export default function give(socket, users) {
     socket.broadcast.to(socket.currentRoom).emit('generalMessage', {
       from: socket.username,
       interaction: ` gives ${giveObj.item.short} to `,
+      target: target.username
+    });
+  });
+
+  socket.on('giveAll', giveObj => {
+    let target = users.find(user => user.username && user.username.toLowerCase() === giveObj.target.toLowerCase());
+    if (!target || target.currentRoom !== socket.currentRoom) return socket.emit('generalMessage', {feedback: 'I don\'t see that person here.'});
+
+    target.emit('getAll', {itemArray: giveObj.itemArray});
+    socket.emit('forceDropAll');
+    socket.emit('generalMessage', {interaction: 'You give everything you\'re carrying to ', target: target.username});
+    socket.broadcast.to(socket.currentRoom).emit('generalMessage', {
+      from: socket.username,
+      interaction: ' gives everything they\'re carrying to ',
       target: target.username
     });
   });
