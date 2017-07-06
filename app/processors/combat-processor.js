57651db23e2180f8b547ebb91dedc2deb220a32b
Steven Bateman <batemanvo@gmail.com>
2017-07-06 12:07:21 -0700

    Reviewed processors and added comments.

--- app/processors/combat-processor.js
+++ app/processors/combat-processor.js
@@ -1,6 +1,7 @@
 'use strict';
 
 export default function combatProcessor(socket, props) {
+  // Pick a random enemy among the targets currently being fought and damage them.
   let rand = Math.floor(Math.random() * props.combat.targets.length);
   let enemy = props.combat.targets[rand];
   socket.emit('damage', {
--- app/processors/terms-processor.js
+++ app/processors/terms-processor.js
@@ -1,6 +1,12 @@
 'use strict';
 
 export default function termsProcessor(searchArray, split) {
+  // This processor searches the terms array on objects to try and find the specified target.
+  // Matches are made starting from the beginning of the string.
+  // For example, if a leather helm has the terms ['leather', 'helm'] and the user enters
+  // GET HEL, then this processor will match the 'helm' element and recognize that the user
+  // is targeting the leather helm. If the user has 2 leather helms, GET 2.HE would be recognized
+  // as the second leather helm.
   let foundStuff;
   let regEx = split.length > 1 ? new RegExp(`^${split[1]}`, 'i') : new RegExp(`^${split[0]}`, 'i');
 
