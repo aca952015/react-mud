1e52870f11d40300dc9cc9dcc12e0aaa376f78b5
Steven Bateman <batemanvo@gmail.com>
2017-07-06 11:42:16 -0700

    Reviewed data and added comments.

--- app/data/mobs.js
+++ app/data/mobs.js
@@ -2,6 +2,10 @@
 
 class Mob {
   constructor(properties) {
+    // Because Object.assign(), for in loops, and various other standard ways of
+    // using constructors only create a shallow copy, we can't use them to properly
+    // instantiate new objects from mobData (since there are nested objects and arrays).
+    // Stringifying, then parsing it is a quick hack to give us a deep clone.
     let someObj = JSON.parse(JSON.stringify(properties));
     someObj.id = Math.floor(Math.random() * 1000000000);
     return someObj;
