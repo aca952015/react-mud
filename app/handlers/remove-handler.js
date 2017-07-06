9978a9b5aa6e59ad6f2ccf23646c4f6c8de8b481
Steven Bateman <batemanvo@gmail.com>
2017-07-06 10:15:42 -0700

    Implemented remove all command.

--- app/handlers/remove-handler.js
+++ app/handlers/remove-handler.js
@@ -8,11 +8,31 @@ import {removeItem} from '../actions/item-actions.js';
 export default function removeHandler(command, args, props) {
   if (!args) return {funcsToCall: [newMessage], feedback: 'Remove what?'};
   args = args.toLowerCase();
-  let equips = Object.keys(props.equipment).map(slot => props.equipment[slot]);
+
+  let equips = Object.keys(props.equipment).reduce((acc, slot) => {
+    if (props.equipment[slot]) acc.push(props.equipment[slot]);
+    return acc;
+  }, []);
+
+  if (args === 'all') {
+    if (!equips.length) return {funcsToCall: [newMessage], feedback: 'You aren\'t wearing anything to remove.'};
+    equips.forEach(item => {
+      let result = generateRemoveObject(item);
+
+      result.funcsToCall.forEach(func => props.dispatch(func(result)));
+      props.socket.emit(result.emitType, result);
+    });
+
+    return {};
+  }
 
   let item = termsProcessor(equips, args.split('.'));
   if (!item) return {funcsToCall: [newMessage], feedback: 'You aren\'t wearing that.'};
 
+  return generateRemoveObject(item);
+}
+
+function generateRemoveObject(item) {
   return {
     funcsToCall: [quietlyAddItem, removeItem, newMessage],
     emitType: 'removeItem',
