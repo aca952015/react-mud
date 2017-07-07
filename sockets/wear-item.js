55767d1499d862fd4e8130b399b8701de9e0a3ea
Steven Bateman <batemanvo@gmail.com>
2017-07-07 16:50:52 -0700

    Fixed a missing property bug in wear socket listener.

--- sockets/wear-item.js
+++ sockets/wear-item.js
@@ -8,7 +8,7 @@ export default function wearItem(socket) {
     // If it's equipment, it's armor. If not, it's a weapon. Use correct grammar accordingly.
     socket.broadcast.to(socket.currentRoom).emit('generalMessage', {
       from: socket.username,
-      feedback: item.type === 'equipment' ? ` equips ${item.equip.short} on their ${item.equip.slot}.`
+      feedback: item.equip.type === 'equipment' ? ` equips ${item.equip.short} on their ${item.equip.slot}.`
                                           : ` equips ${item.equip.short} in their ${item.equip.slot}.`
     });
   });
