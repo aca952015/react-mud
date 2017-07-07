f6a6c656b50746c7f5ef7a0e0ae8f88a709a36ab
Steven Bateman <batemanvo@gmail.com>
2017-07-07 12:18:48 -0700

    Added the functionality of reducing damage by the total def on armor.

--- sockets/mob-target-selector.js
+++ sockets/mob-target-selector.js
@@ -5,12 +5,23 @@ export default function mobTargetSelector(mobsInCombat, users) {
   // Emit a damage event to the target and a combatLog event to everyone else
   // in the room.
   for (let i = 0; i < mobsInCombat.length; i++) {
-    let targets = mobsInCombat[i].combat.targets;
-    let selectedTarget = targets[Math.floor(Math.random() * targets.length)];
-    let socket = users.find(user => user.username === selectedTarget);
+    const targets = mobsInCombat[i].combat.targets;
+    const selectedTarget = targets[Math.floor(Math.random() * targets.length)];
+    const socket = users.find(user => user.username === selectedTarget);
+
+    // Get the def stat of all equipped items and add them together
+    const defense = Object.keys(socket.equipment).reduce((acc, slot) => {
+      if (socket.equipment[slot]) acc += socket.equipment[slot].stats.def;
+      return acc;
+    }, 0);
+
+    // Mobs must do at least 1 damage
+    let damage = mobsInCombat[i].atk - defense;
+    if (damage < 1) damage = 1;
+
     socket.emit('damage', {
       enemy: mobsInCombat[i],
-      damage: mobsInCombat[i].atk
+      damage
     });
     let mob = mobsInCombat[i];
     socket.broadcast.to(socket.currentRoom).emit('generalMessage', {
