1b5aa3ac38fd8e976c719b0d91bb1734c43e5a13
Steven Bateman <batemanvo@gmail.com>
2017-07-07 12:19:27 -0700

    Added a test for damage with no armor and damage with enough armor to reduce below 1.

--- test/sockets/mob-target-selector.test.js
+++ test/sockets/mob-target-selector.test.js
@@ -4,10 +4,18 @@ import io from 'socket.io-client';
 import ioOptions from '../lib/io-options.js';
 import closeServer from '../lib/test-server.js';
 import newMob from '../../app/data/mobs.js';
+import newItem from '../../app/data/items.js';
 
 describe('mobTargetSelector', () => {
   let player1, player2;
   let bat = newMob('bat');
+  let equipment = {
+    head: null,
+    shoulders: null,
+    chest: null,
+    legs: null,
+    feet: null
+  };
   require('../lib/test-server.js');
 
   beforeEach(done => {
@@ -34,19 +42,56 @@ describe('mobTargetSelector', () => {
     done();
   });
 
-  it('should pick a target from a mob\'s current targets and emit a damage event', done => {
-    player1.emit('testMobSelector');
-    player1.on('damage', res => {
-      expect(res.enemy).toEqual({
-        ...bat,
-        id: res.enemy.id,
-        combat: {
-          active: true,
-          targets: ['player1'] // Check that player2 didn't get drawn into combat somehow
-        }
+  describe('With no armor equipped', () => {
+    beforeEach(done => {
+      player1.emit('updateEquipment', equipment);
+      player1.emit('updateSocket');
+      player1.on('updateComplete', () => done());
+    });
+
+    it('should pick a target from a mob\'s current targets and emit a damage event', done => {
+      player1.emit('testMobSelector');
+      player1.on('damage', res => {
+        expect(res.enemy).toEqual({
+          ...bat,
+          id: res.enemy.id,
+          combat: {
+            active: true,
+            targets: ['player1'] // Check that player2 didn't get drawn into combat somehow
+          }
+        });
+        expect(res.damage).toEqual(bat.atk);
+        done();
       });
-      expect(res.damage).toEqual(bat.atk);
-      done();
+    });
+  });
+
+  describe('With enough armor equipped to reduce the damage to less than 1', () => {
+    beforeEach(done => {
+      player1.emit('updateEquipment', {
+        ...equipment,
+        head: newItem('equipment', 'leather helm'),
+        chest: newItem('equipment', 'leather breastplate'),
+        feet: newItem('equipment', 'leather boots')
+      });
+      player1.emit('updateSocket');
+      player1.on('updateComplete', () => done());
+    });
+
+    it('should reduce the damage to 1', done => {
+      player1.emit('testMobSelector');
+      player1.on('damage', res => {
+        expect(res.enemy).toEqual({
+          ...bat,
+          id: res.enemy.id,
+          combat: {
+            active: true,
+            targets: ['player1']
+          }
+        });
+        expect(res.damage).toEqual(1);
+        done();
+      });
     });
   });
 });
