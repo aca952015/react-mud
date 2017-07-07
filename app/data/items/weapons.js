e41403a48af78edfcc7cb17d425f8a9070308c89
Steven Bateman <batemanvo@gmail.com>
2017-07-07 16:45:14 -0700

    Added weapons to itemData with a test sword.

---
+++ app/data/items/weapons.js
@@ -0,0 +1,17 @@
+'use strict';
+
+export const weapons = {
+  'broad sword': {
+    name: 'broad sword',
+    short: 'a broad sword',
+    long: 'A simple metal sword with a wide, flat blade lies on the ground here.',
+    terms: ['metal', 'sword', 'broad', 'simple'],
+    type: 'weapons',
+    drink: null,
+    description: 'This is a placeholder description for a broad sword.',
+    slot: 'main hand',
+    stats: {
+      atk: 3
+    }
+  }
+};
