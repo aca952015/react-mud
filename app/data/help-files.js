8cdfa9dab6660357d275019a3dcd7b22be8e42f7
Steven Bateman <batemanvo@gmail.com>
2017-07-08 14:37:36 -0700

    Added inline-block functionality to the help topics again.

--- app/data/help-files.js
+++ app/data/help-files.js
@@ -4,25 +4,25 @@ export const helpFile = {
   'help': {
     title: 'Here is a list of help files available to you. Type help {file} to learn more (e.g., "HELP MOVEMENT"):',
     text: [
-      'DRINK',
-      'DROP',
-      'EQUIPMENT',
-      'EXAMINE',
-      'GET',
-      'GIVE',
-      'INVENTORY',
-      'KILL',
-      'LOCK',
-      'LOOK',
-      'MOVEMENT',
-      'PREVIOUS_COMMANDS',
-      'PUT',
-      'REMOVE',
-      'SAY',
-      'UNLOCK',
-      'WEAR',
-      'WHISPER',
-      'WHO'
+      `<span class="help-topics">DRINK</span>
+      <span class="help-topics">DROP</span>
+      <span class="help-topics">EQUIPMENT</span>
+      <span class="help-topics">EXAMINE</span>
+      <span class="help-topics">GET</span>
+      <span class="help-topics">GIVE</span>
+      <span class="help-topics">INVENTORY</span>
+      <span class="help-topics">KILL</span>
+      <span class="help-topics">LOCK</span>
+      <span class="help-topics">LOOK</span>
+      <span class="help-topics">MOVEMENT</span>
+      <span class="help-topics">PREVIOUS_COMMANDS</span>
+      <span class="help-topics">PUT</span>
+      <span class="help-topics">REMOVE</span>
+      <span class="help-topics">SAY</span>
+      <span class="help-topics">UNLOCK</span>
+      <span class="help-topics">WEAR</span>
+      <span class="help-topics">WHISPER</span>
+      <span class="help-topics">WHO</span>`
     ]
   },
   'drink': {
