36cd7f8457831025760425f212314825af701972
Steven Bateman <batemanvo@gmail.com>
2017-07-09 12:13:04 -0700

    Added a user action test for ADD_DESCRIPTION_PARAGRAPH.

--- test/actions/user-actions.test.js
+++ test/actions/user-actions.test.js
@@ -8,4 +8,10 @@ describe('User actions', () => {
       expect(truncateDescription()).toEqual({type: 'TRUNCATE_DESCRIPTION'});
     });
   });
+
+  describe('addDescriptionParagraph', () => {
+    it('should return an object with a type of TRUNCATE_DESCRIPTION and a payload of the last element of a playerDescription', () => {
+      expect(addDescriptionParagraph({playerDescription: ['dude', 'berb']})).toEqual({type: 'ADD_DESCRIPTION_PARAGRAPH', payload: 'berb'});
+    });
+  });
 });
