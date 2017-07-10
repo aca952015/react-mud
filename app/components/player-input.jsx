821eddf91af5249b1d2e80990991dd6a537ce5d7
Steven Bateman <batemanvo@gmail.com>
2017-07-10 16:23:44 -0700

    Changed playerInput to not give feedback during login.

--- app/components/player-input.jsx
+++ app/components/player-input.jsx
@@ -4,11 +4,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const PlayerInput = props => {
+  if (props.currentRoom === 'Login Room') return;
   return <div className="player-input">
     <p>{props.message.playerInput}</p>
   </div>;
 };
 
 PlayerInput.propTypes = {
-  message: PropTypes.object
+  message: PropTypes.object,
+  currentRoom: PropTypes.string
 };
