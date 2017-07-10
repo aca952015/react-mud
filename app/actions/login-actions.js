76657add03a5a3ab2cdf5b53810cb397a2d9dec0
Steven Bateman <batemanvo@gmail.com>
2017-07-10 15:43:07 -0700

    Created login actions.

---
+++ app/actions/login-actions.js
@@ -0,0 +1,34 @@
+'use strict';
+
+export function incrementCreationStep() {
+  return {type: 'INCREMENT_CREATION_STEP'};
+}
+
+export function createNew() {
+  return {type: 'NEW_CHARACTER'};
+}
+
+export function endCreation() {
+  return {type: 'CHARACTER_COMPLETE'};
+}
+
+export function setUsername(username) {
+  return {
+    type: 'SET_USERNAME',
+    payload: username
+  };
+}
+
+export function setFirstPassword(password) {
+  return {
+    type: 'SET_FIRST_PASSWORD',
+    payload: password.firstPassword
+  };
+}
+
+export function setCreationStep(step) {
+  return {
+    type: 'SET_CREATION_STEP',
+    payload: step.step
+  };
+}
