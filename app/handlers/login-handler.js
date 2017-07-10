c3b2105c7d25863cc4c0ed1b2a95758bd2ce86cc
Steven Bateman <batemanvo@gmail.com>
2017-07-10 16:24:14 -0700

    Added login functionality for an existing character to loginHandler.

--- app/handlers/login-handler.js
+++ app/handlers/login-handler.js
@@ -1,24 +1,32 @@
 'use strict';
 
 import {newMessage} from '../actions/message-actions.js';
-import {createNew, setFirstPassword, incrementCreationStep, setCreationStep, endCreation} from '../actions/login-actions.js';
+import {createNew, setFirstPassword, incrementCreationStep, setUsername, setCreationStep} from '../actions/login-actions.js';
 
 export default function loginHandler(command, args, props) {
   if (props.creatingNew) {
+    if (command === 'new') return {funcsToCall: [newMessage], feedback: 'You\'re already in the process of making a character.'};
     if (props.creationStep === 0) return {emitType: 'checkUsername', newUsername: command};
     if (props.creationStep === 1) return {funcsToCall: [newMessage, incrementCreationStep, setFirstPassword], firstPassword: command, feedback: 'Confirm password.'};
     if (props.creationStep === 2) {
       if (command !== props.firstPassword) return {funcsToCall: [newMessage, setCreationStep], step: 1, feedback: 'Passwords don\'t match. Please enter a new password.'};
-      return {emitType: 'createCharacter', funcsToCall: [endCreation], newUsername: props.newUsername, password: command};
+      return {emitType: 'createCharacter', newUsername: props.newUsername, password: command};
     }
   }
 
   if (command === 'new') {
-    if (props.creatingNew) return {funcsToCall: [newMessage], feedback: 'You\'re already in the process of making a character.'};
-
     return {
       funcsToCall: [newMessage, createNew],
       feedback: 'Please enter a name for your character.'
+    };
+  }
+
+  if (props.creationStep === 0) return {funcsToCall: [newMessage, incrementCreationStep, setUsername], newUsername: command, feedback: 'Enter the password for that character.'};
+  if (props.creationStep === 1) {
+    return {
+      emitType: 'login',
+      username: props.newUsername,
+      password: command
     };
   }
 }
