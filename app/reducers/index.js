8820ee01eadfc891dc1a228a10728fb83ceb8570
Steven Bateman <batemanvo@gmail.com>
2017-07-10 15:40:02 -0700

    Added a login reducer.

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -4,9 +4,11 @@ import {combineReducers} from 'redux';
 import user from './user-reducer.js';
 import messages from './message-reducer.js';
 import equipment from './equipment-reducer.js';
+import login from './login-reducer.js';
 
 export default combineReducers({
   user,
   messages,
-  equipment
+  equipment,
+  login
 });
---
+++ app/reducers/login-reducer.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const initialState = {
+  creatingNew: false,
+  creationStep: 0,
+  newUsername: 'default',
+  firstPassword: 'default'
+};
+
+export default function reducer(state=initialState, action) {
+  if (action.type === 'NEW_CHARACTER') return {...state, creatingNew: true};
+  if (action.type === 'SET_USERNAME') return {...state, newUsername: action.payload};
+  if (action.type === 'SET_FIRST_PASSWORD') return {...state, firstPassword: action.payload};
+  if (action.type === 'SET_CREATION_STEP') return {...state, creationStep: action.payload};
+  if (action.type === 'CHARACTER_COMPLETE') return {...state, creatingNew: false};
+  if (action.type === 'INCREMENT_CREATION_STEP') {
+    const newStep = state.creationStep + 1;
+    return {...state, creationStep: newStep};
+  }
+  return state;
+}
