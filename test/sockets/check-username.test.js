ee7eee7cbb129553e8e6dbb112a3f906b65b1603
Steven Bateman <batemanvo@gmail.com>
2017-07-11 13:54:33 -0700

    Added a test for check username with a taken name.

--- test/sockets/check-username.test.js
+++ test/sockets/check-username.test.js
@@ -4,6 +4,8 @@ import io from 'socket.io-client';
 import {Character} from '../../model/character.js';
 import closeServer from '../lib/test-server.js';
 import {ioOptions} from '../lib/io-options.js';
+import {initialState as user} from '../../app/data/user-initial-state.js';
+import {initialState as equipment} from '../../app/data/equipment-initial-state.js';
 
 describe('check username', () => {
   let player1;
@@ -32,6 +34,21 @@ describe('check username', () => {
       player1.emit('checkUsername', {newUsername: 'Bob'});
       player1.on('nameAvailable', res => {
         expect(res).toEqual('Bob');
+        done();
+      });
+    });
+  });
+
+  describe('With a name taken', () => {
+    beforeEach(done => {
+      new Character({password: 'banana', ...user, username: 'Bob', equipment: {...equipment}}).save()
+      .then(done);
+    });
+
+    it('should return a generalMessage event', done => {
+      player1.emit('checkUsername', {newUsername: 'Bob'});
+      player1.on('generalMessage', res => {
+        expect(res.feedback).toEqual('That name is taken. Please select a different name.');
         done();
       });
     });
