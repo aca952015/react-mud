e831d7431dd1ae9e4179d25712880b8a11edfe1d
Steven Bateman <batemanvo@gmail.com>
2017-07-11 11:05:55 -0700

    Added login reducer test for action SET_USERNAME.

--- test/reducers/login-reducer.test.js
+++ test/reducers/login-reducer.test.js
@@ -8,12 +8,21 @@ describe('login reducer', () => {
       expect(reducer(undefined, {})).toEqual(initialState);
     });
   });
-  
+
   describe('With action of NEW_CHARACTER', () => {
     it('should change creatingNew to true', () => {
       expect(reducer(initialState, {type: 'NEW_CHARACTER'})).toEqual({
         ...initialState,
         creatingNew: true
+      });
+    });
+  });
+
+  describe('With action of SET_USERNAME', () => {
+    it('should update the newUsername of state with the payload', () => {
+      expect(reducer(initialState, {type: 'SET_USERNAME', payload: 'Dave'})).toEqual({
+        ...initialState,
+        newUsername: 'Dave'
       });
     });
   });
