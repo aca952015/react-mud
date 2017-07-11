c0bc40e814951b85db0de0bddf85fe6467f217f1
Steven Bateman <batemanvo@gmail.com>
2017-07-10 18:44:15 -0700

    Modified initial connect to make use of initialStates from data.

--- sockets/initial-connect.js
+++ sockets/initial-connect.js
@@ -1,32 +1,14 @@
 'use strict';
 
+import {initialState as user} from '../app/data/user-initial-state.js';
+import {initialState as equipment} from '../app/data/equipment-initial-state.js';
+
 export default function initialConnect(socket) {
   socket.emit('initialConnect', {
-    username: `Robot_${Math.floor(Math.random() * 500 + 1)}`,
-    description: ['As actual players do not exist yet, everybody is a robot. They all look the same. They all speak the same. They look just like you.'],
-    inventory: [],
-    hp: 15,
-    maxHP: 20,
-    mp: 11,
-    maxMP: 20,
-    level: 1,
-    atk: 2,
-    def: 0,
-    mat: 0,
-    mdf: 0,
-    currentRoom: 'Login Room',
-    combat: {
-      active: false,
-      targets: []
+    user: {
+      ...user,
+      currentRoom: 'Login Room'
     },
-    equipment: {
-      head: null,
-      shoulders: null,
-      'main hand': null,
-      'off hand': null,
-      chest: null,
-      legs: null,
-      feet: null
-    }
+    equipment
   });
 }
