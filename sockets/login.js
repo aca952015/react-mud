631373266e078ec1db183bd344dd91918a987255
Steven Bateman <batemanvo@gmail.com>
2017-07-10 18:44:50 -0700

    Modified login listener to set equipment properties in a specific order.

--- sockets/login.js
+++ sockets/login.js
@@ -7,14 +7,25 @@ export default function login(socket) {
     Character.findOne({username: auth.username.toLowerCase()})
     .then(char => char.validatePassword(auth.password))
     .then(char => {
-      const loginUser = JSON.parse(JSON.stringify(char));
+      const loginUser = char;
       delete loginUser.equipment;
       delete loginUser.password;
       loginUser.username = `${loginUser.username[0].toUpperCase()}${loginUser.username.slice(1)}`;
 
-      const loginEquipment = char.equipment;
+      // For some reason, the properties kept inverting themselves every other login - e.g., slots
+      // would be displayed from the feet up instead of the head down. Manually setting the
+      // properties here fixes the problem, though I don't see the difference yet.
+      const equipment = {
+        head: char.equipment.head,
+        shoulders: char.equipment.shoulders,
+        'main hand': char.equipment['main hand'],
+        'off hand': char.equipment['off hand'],
+        chest: char.equipment.chest,
+        legs: char.equipment.legs,
+        feet: char.equipment.feet
+      };
 
-      socket.emit('loginSuccessful', {loginUser, loginEquipment});
+      socket.emit('loginSuccessful', {loginUser, loginEquipment: equipment});
     })
     .catch(() => socket.emit('loginFail'));
   });
