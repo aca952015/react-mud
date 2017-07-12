b2b5e5f74996c2762144327ed734ef774c41b840
Steven Bateman <batemanvo@gmail.com>
2017-07-12 16:16:47 -0700

    Updated respawnItem for timers at 0 with mobs.

--- test/server_helpers/respawn-item.test.js
+++ test/server_helpers/respawn-item.test.js
@@ -6,31 +6,33 @@ import {roomData} from '../../app/data/rooms.js';
 describe('respawnItems', () => {
   const roomReset = JSON.parse(JSON.stringify(roomData));
 
-  describe('With a room not missing anything', () => {
+  describe('With a room not missing any items or mobs', () => {
     it('should maintain resetTimers of 0 and have the same item array as started', () => {
       respawnItems(roomData, roomReset);
-      expect(roomData['Nexus'].resetTimer).toEqual(0);
+      expect(roomData['Nexus'].itemResetTimer).toEqual(0);
       expect(roomData['Nexus'].items).toEqual(roomReset['Nexus'].items);
+      expect(roomData['Nexus'].mobResetTimer).toEqual(0);
+      expect(roomData['Nexus'].mobs).toEqual(roomReset['Nexus'].mobs);
     });
   });
 
-  describe('With a room missing something', () => {
+  describe('With a room missing some items', () => {
     describe('The first tick it\'s missing', () => {
       it('should increase resetTimer to 1, but leave the items the same', () => {
         roomData['Nexus'].items.splice(0, 1);
         respawnItems(roomData, roomReset);
-        expect(roomData['Nexus'].resetTimer).toEqual(1);
+        expect(roomData['Nexus'].itemResetTimer).toEqual(1);
         expect(roomData['Nexus'].items).toEqual(roomReset['Nexus'].items.slice(1));
       });
     });
 
-    describe('When the timer has gone over 3', () => {
+    describe('When the timer has gone over 3 with missing items', () => {
       it('should respawn any missing items and set the resetTimer back to 0', () => {
-        roomData['Nexus'].resetTimer = 3;
+        roomData['Nexus'].itemResetTimer = 3;
         respawnItems(roomData, roomReset);
         expect(roomData['Nexus'].items[roomData['Nexus'].items.length - 1].name).toEqual('corpse');
         expect(roomData['Nexus'].items[roomData['Nexus'].items.length - 1].short).toEqual('a corpse');
-        expect(roomData['Nexus'].resetTimer).toEqual(0);
+        expect(roomData['Nexus'].itemResetTimer).toEqual(0);
       });
     });
   });
