a8d52400f93942406f0d17e65acfd002bdd71ea4
Steven Bateman <batemanvo@gmail.com>
2017-07-12 11:04:45 -0700

    Added a client tick listener to socket handlers.

--- app/client_sockets/socket-handlers.js
+++ app/client_sockets/socket-handlers.js
@@ -5,6 +5,7 @@ import commsHandlers from './comms-handlers.js';
 import moveListeners from './move-listeners.js';
 import inventoryHandlers from './inventory-handlers.js';
 import combatHandlers from './combat-handlers.js';
+import tickListeners from './tick-listeners.js';
 
 export default function socketHandlers(homeCtx) {
   loginHandlers(homeCtx);
@@ -12,4 +13,5 @@ export default function socketHandlers(homeCtx) {
   moveListeners(homeCtx);
   inventoryHandlers(homeCtx);
   combatHandlers(homeCtx);
+  tickListeners(homeCtx);
 }
