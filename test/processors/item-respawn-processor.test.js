cd5c04a9675d27e55eb357b751f86a7b46023aab
Steven Bateman <batemanvo@gmail.com>
2017-07-12 15:38:05 -0700

    Added an itemRespawnProcessor test for extra items.

--- test/processors/item-respawn-processor.test.js
+++ test/processors/item-respawn-processor.test.js
@@ -4,11 +4,18 @@ import itemRespawnProcessor from '../../app/processors/item-respawn-processor.js
 import newItem from '../../app/data/items.js';
 
 describe('item respawn processor', () => {
+  let originalRoom = [newItem('equipment', 'leather helm'), newItem('keys', 'gallows key')];
+  let currentRoom = [newItem('keys', 'gallows key'), newItem('equipment', 'leather helm')];
+
   describe('With a room that isn\'t missing any items', () => {
     it('should return an empty array', () => {
-      let originalRoom = [newItem('equipment', 'leather helm'), newItem('keys', 'gallows key')];
-      let currentRoom = [newItem('keys', 'gallows key'), newItem('equipment', 'leather helm')];
       expect(itemRespawnProcessor(originalRoom, currentRoom)).toEqual([]);
+    });
+  });
+
+  describe('With a room that has extra items', () => {
+    it('should return an empty array', () => {
+      expect(itemRespawnProcessor(originalRoom, [...currentRoom, newItem('potions', 'health potion')])).toEqual([]);
     });
   });
 });
