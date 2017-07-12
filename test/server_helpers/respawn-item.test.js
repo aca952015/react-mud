6af050cf0950f72f3c60074d6697c5437040ba29
Steven Bateman <batemanvo@gmail.com>
2017-07-12 16:21:09 -0700

    Updated respawnItem test to also spawn mobs.

--- test/server_helpers/respawn-item.test.js
+++ test/server_helpers/respawn-item.test.js
@@ -16,23 +16,30 @@ describe('respawnItems', () => {
     });
   });
 
-  describe('With a room missing some items', () => {
+  describe('With a room missing some items and mobs', () => {
     describe('The first tick it\'s missing', () => {
       it('should increase resetTimer to 1, but leave the items the same', () => {
         roomData['Nexus'].items.splice(0, 1);
+        roomData['Nexus'].mobs.splice(0, 1);
         respawnItems(roomData, roomReset);
         expect(roomData['Nexus'].itemResetTimer).toEqual(1);
+        expect(roomData['Nexus'].mobResetTimer).toEqual(1);
         expect(roomData['Nexus'].items).toEqual(roomReset['Nexus'].items.slice(1));
+        expect(roomData['Nexus'].mobs).toEqual(roomReset['Nexus'].mobs.slice(1));
       });
     });
 
-    describe('When the timer has gone over 3 with missing items', () => {
+    describe('When the timer has gone over 3 with missing items and mobs', () => {
       it('should respawn any missing items and set the resetTimer back to 0', () => {
         roomData['Nexus'].itemResetTimer = 3;
+        roomData['Nexus'].mobResetTimer = 3;
         respawnItems(roomData, roomReset);
         expect(roomData['Nexus'].items[roomData['Nexus'].items.length - 1].name).toEqual('corpse');
         expect(roomData['Nexus'].items[roomData['Nexus'].items.length - 1].short).toEqual('a corpse');
         expect(roomData['Nexus'].itemResetTimer).toEqual(0);
+        expect(roomData['Nexus'].mobs[roomData['Nexus'].mobs.length - 1].name).toEqual('bat');
+        expect(roomData['Nexus'].mobs[roomData['Nexus'].mobs.length - 1].short).toEqual('a small bat');
+        expect(roomData['Nexus'].mobResetTimer).toEqual(0);
       });
     });
   });
