d80d62eaa17077f22a993588c53171043d6ca0da
Steven Bateman <batemanvo@gmail.com>
2017-07-13 14:31:07 -0700

    Added a check for death on resurrect listener.

--- sockets/resurrect.js
+++ sockets/resurrect.js
@@ -4,6 +4,7 @@ export default function resurrect(socket, roomData) {
   socket.on('resurrect', () => {
     let healer = roomData[socket.currentRoom].mobs.find(mob => mob.name === 'healer');
     if (!healer) return socket.emit('generalMessage', {feedback: 'There\'s no one here to resurrect you.'});
+    if (!socket.effects.death) return socket.emit('generalMessage', {feedback: 'You aren\'t dead.'});
 
     socket.emit('generalMessage', {from: `${healer.short[0].toUpperCase()}${healer.short.slice(1)}`, interaction: ' closes her eyes, utters a blessing, and holds her palms above ', target: 'you'});
     socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: `${healer.short[0].toUpperCase()}${healer.short.slice(1)}`, interaction: ' closes her eyes, utters a blessing, and holds her palms above ', target: socket.username});
