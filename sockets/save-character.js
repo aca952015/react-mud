d2fe7f267adb9e9d23d55f947eb1607dff7660cb
Steven Bateman <batemanvo@gmail.com>
2017-07-13 14:31:47 -0700

    Amended save character listener for new effects property.

--- sockets/save-character.js
+++ sockets/save-character.js
@@ -4,8 +4,16 @@ import {Character} from '../model/character.js';
 
 export default function saveCharacter(socket) {
   socket.on('saveCharacter', character => {
-    Character.findByIdAndUpdate(character._id, {...character, username: character.username.toLowerCase()})
+    Character.findById(character._id)
+    .then(char => {
+      // findByIdAndUpdate does not work on effects, so it must be saved using the markModified and
+      // manual save functions.
+      char.effects = character.effects;
+      char.markModified('effects');
+      return char.save();
+    })
+    .then(char => Character.findByIdAndUpdate(char._id, {...character, username: character.username.toLowerCase()}))
     .then(() => socket.emit('generalMessage', {feedback: 'Character saved.'}))
-    .catch(err => socket.emit('generalMessage', {err, feedback: 'Error saving character.'}));
+    .catch(err => socket.emit('generalMessage', {err, feedback: `Error saving character: ${err.message}.`}));
   });
 }
