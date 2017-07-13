b4574d6adc16f372cf1452eab81c34fa536883b7
Steven Bateman <batemanvo@gmail.com>
2017-07-13 14:30:39 -0700

    Added a resurrect socket listener.

---
+++ sockets/resurrect.js
@@ -0,0 +1,18 @@
+'use strict';
+
+export default function resurrect(socket, roomData) {
+  socket.on('resurrect', () => {
+    let healer = roomData[socket.currentRoom].mobs.find(mob => mob.name === 'healer');
+    if (!healer) return socket.emit('generalMessage', {feedback: 'There\'s no one here to resurrect you.'});
+
+    socket.emit('generalMessage', {from: `${healer.short[0].toUpperCase()}${healer.short.slice(1)}`, interaction: ' closes her eyes, utters a blessing, and holds her palms above ', target: 'you'});
+    socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: `${healer.short[0].toUpperCase()}${healer.short.slice(1)}`, interaction: ' closes her eyes, utters a blessing, and holds her palms above ', target: socket.username});
+
+    socket.emit('generalMessage', {interaction: 'A bright column of light washes over ', target: 'you'});
+    socket.broadcast.to(socket.currentRoom).emit('generalMessage', {interaction: 'A bright column of light washes over ', target: socket.username});
+
+    socket.emit('resurrect');
+    socket.emit('generalMessage', {feedback: 'You have been RESURRECTED!'});
+    socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, feedback: ' has been resurrected!'});
+  });
+}
