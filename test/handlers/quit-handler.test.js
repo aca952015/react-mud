ec7ab6e76f2219e8d75acec40a04dd0997d38653
Steven Bateman <batemanvo@gmail.com>
2017-07-14 16:16:02 -0700

    Added a quitHandler test for quit out of combat.

--- test/handlers/quit-handler.test.js
+++ test/handlers/quit-handler.test.js
@@ -1,5 +1,8 @@
 'use strict';
 
+import {initialState as user} from '../../app/data/user-initial-state.js';
+import {initialState as equipment} from '../../app/data/equipment-initial-state.js';
+import {loginUser, loginEquipment, loginEffects} from '../../app/actions/login-actions.js';
 import {newMessage} from '../../app/actions/message-actions.js';
 import quitHandler from '../../app/handlers/quit-handler.js';
 
@@ -9,6 +12,18 @@ describe('quitHandler', () => {
       expect(quitHandler('quit', undefined, {combat: {active: true}})).toEqual({
         funcsToCall: [newMessage],
         feedback: 'You can\'t quit while you\'re in combat!'
+      });
+    });
+  });
+
+  describe('If the user is not in combat', () => {
+    it('should return a quit object', () => {
+      expect(quitHandler('quit', undefined, {combat: {active: false}})).toEqual({
+        emitType: 'quit',
+        funcsToCall: [loginUser, loginEquipment, loginEffects],
+        loginUser: user,
+        loginEquipment: equipment,
+        loginEffects: {}
       });
     });
   });
