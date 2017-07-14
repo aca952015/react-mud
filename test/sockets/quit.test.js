0cb7980c2f7b9d837ac2387476c5cb2ef1dfec09
Steven Bateman <batemanvo@gmail.com>
2017-07-14 16:28:54 -0700

    Added a test for quit properly giving the player the right room info.

--- test/sockets/quit.test.js
+++ test/sockets/quit.test.js
@@ -3,6 +3,7 @@
 import io from 'socket.io-client';
 import closeServer from '../lib/test-server.js';
 import ioOptions from '../lib/io-options';
+import {roomData} from '../../app/data/rooms.js';
 
 describe('Quit socket listener', () => {
   let player1;
@@ -28,6 +29,21 @@ describe('Quit socket listener', () => {
     player1.emit('quit');
     player1.on('move', res => {
       expect(res).toEqual('Login Room');
+      done();
+    });
+  });
+
+  it('should give the player the room contents of Login Room', done => {
+    player1.emit('quit');
+    player1.on('generalMessage', res => {
+      expect(res.occupants).toEqual([]);
+      expect(res.mobs).toEqual(null);
+      expect(res.room).toEqual({
+        roomName: 'Login Room',
+        desc: roomData['Login Room'].desc,
+        exits: roomData['Login Room'].exits,
+        items: roomData['Login Room'].items
+      });
       done();
     });
   });
