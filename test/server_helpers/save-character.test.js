7a0879a6a5e5ca50f46968fbafcadcc0cb5273e2
Steven Bateman <batemanvo@gmail.com>
2017-07-14 16:21:04 -0700

    Added a test for lib saveCharacter with a home context.

---
+++ test/server_helpers/save-character.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+import sinon from 'sinon';
+import saveCharacter from '../../lib/save-character.js';
+
+describe('saveCharacter', () => {
+  let homeCtx = {
+    socket: {
+      emit: sinon.spy()
+    },
+    props: {
+      user: 'Dude',
+      equipment: 'Equipment',
+      effects: 'Effects'
+    }
+  };
+
+  describe('From the home component', () => {
+    it('should get the socket directly from homeCtx', () => {
+      saveCharacter(homeCtx);
+      expect(homeCtx.socket.emit.calledWith('saveCharacter', {
+        ...homeCtx.props.user,
+        equipment: homeCtx.props.equipment,
+        effects: homeCtx.props.effects
+      })).toEqual(true);
+      expect(homeCtx.socket.emit.calledWith('escapeCombat')).toEqual(true);
+      expect(homeCtx.socket.emit.calledWith('disconnect')).toEqual(true);
+    });
+  });
+});
