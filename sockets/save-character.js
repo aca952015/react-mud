64f06d2c4315d5083c40d37ab8e8a122ad982ec4
Steven Bateman <batemanvo@gmail.com>
2017-07-14 15:51:02 -0700

    Added 'vanishes into the nether' functionality to the save-character socket.

--- sockets/save-character.js
+++ sockets/save-character.js
@@ -4,10 +4,11 @@ import {Character} from '../model/character.js';
 
 export default function disconnect(socket, users) {
   socket.on('saveCharacter', character => {
+    socket.broadcast.to(socket.currentRoom).emit('generalMessage', {from: socket.username, feedback: ' vanishes into the nether.'});
     Character.findById(character._id)
     .then(char => {
       delete character._id; // Used to prevent issues of trying to overwrite IDs and getting errors
-      
+
       // findByIdAndUpdate does not work on effects, so it must be saved using the markModified and
       // manual save functions.
       char.effects = character.effects;
