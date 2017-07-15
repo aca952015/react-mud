3797b1aa4016f8e1e992fc56294fe6e9053267e6
Steven Bateman <batemanvo@gmail.com>
2017-07-15 13:31:42 -0700

    Added GCD action to skill reducer.

--- app/reducers/skill-reducer.js
+++ app/reducers/skill-reducer.js
@@ -2,7 +2,7 @@
 
 import {classSkills} from '../data/class-skills.js';
 
-export default function reducer(state={}, action) {
+export default function reducer(state={...classSkills['warriorSkills']}, action) {
   if (action.type === 'SET_SKILLS') return {...classSkills[action.payload]};
   if (action.type === 'ON_COOLDOWN') {
     let newState = {...state};
@@ -14,5 +14,7 @@ export default function reducer(state={}, action) {
     newState[action.payload].onCooldown = false;
     return newState;
   }
+  if (action.type === 'START_GLOBAL_COOLDOWN') return {...state, globalCooldown: true};
+  if (action.type === 'END_GLOBAL_COOLDOWN') return {...state, globalCooldown: false};
   return {...state};
 }
