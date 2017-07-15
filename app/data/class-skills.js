7bc822e912d67f8ca1c218e3340e414eae5c1de6
Steven Bateman <batemanvo@gmail.com>
2017-07-15 11:31:49 -0700

    Added a class skills data object.

---
+++ app/data/class-skills.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import {warriorSkills} from './skills/warrior-skills.js';
+
+export const classSkills = {
+  warriorSkills
+};
---
+++ app/data/skills/warrior-skills.js
@@ -0,0 +1,7 @@
+'use strict';
+
+export const warriorSkills = {
+  slash: {
+    onCooldown: false
+  }
+};
