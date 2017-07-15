0cdecbc446123f0dc1ed2a65beae8fa956a23b83
Steven Bateman <batemanvo@gmail.com>
2017-07-15 11:23:36 -0700

    Created a test for skill action endCooldown.

--- test/actions/skill-actions.test.js
+++ test/actions/skill-actions.test.js
@@ -8,4 +8,10 @@ describe('skill actions', () => {
       expect(startCooldown('slash')).toEqual({type: 'ON_COOLDOWN', payload: 'slash'});
     });
   });
+
+  describe('endCooldown', () => {
+    it('should return an object with type "OFF_COOLDOWN" and payload of the name passed in', () => {
+      expect(endCooldown('slash')).toEqual({type: 'OFF_COOLDOWN', payload: 'slash'});
+    });
+  });
 });
