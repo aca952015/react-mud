f730aeeda4ed768f7959514551d25fe60b01f09a
Steven Bateman <batemanvo@gmail.com>
2017-07-15 11:25:05 -0700

    Added a test for initial state of the skills reducer.

--- test/reducers/skills-reducer.test.js
+++ test/reducers/skills-reducer.test.js
@@ -1,1 +1,9 @@
 'use strict';
+
+import reducer from '../../app/reducers/skill-reducer.js';
+
+describe('skills reducer', () => {
+  it('should have an initialState of an empty object', () => {
+    expect(reducer(undefined, {})).toEqual({});
+  });
+});
