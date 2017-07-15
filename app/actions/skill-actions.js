17757cd714578c7eadf65f236a49763207d9c71a
Steven Bateman <batemanvo@gmail.com>
2017-07-15 13:27:44 -0700

    Created more skill actions for global cooldowns.

--- app/actions/skill-actions.js
+++ app/actions/skill-actions.js
@@ -3,15 +3,23 @@
 export function startCooldown(skillName) {
   return {
     type: 'ON_COOLDOWN',
-    payload: skillName
+    payload: skillName.skillName
   };
 }
 
 export function endCooldown(skillName) {
   return {
     type: 'OFF_COOLDOWN',
-    payload: skillName
+    payload: skillName.skillName
   };
+}
+
+export function startGlobalCooldown() {
+  return {type: 'START_GLOBAL_COOLDOWN'};
+}
+
+export function endGlobalCooldown() {
+  return {type: 'END_GLOBAL_COOLDOWN'};
 }
 
 export function setSkills(classSkills) {
