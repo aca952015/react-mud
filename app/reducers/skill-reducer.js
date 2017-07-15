e1cf1d94f5c7ba4b036c96c4c570345f3bec0572
Steven Bateman <batemanvo@gmail.com>
2017-07-15 11:32:07 -0700

    Added a skill reducer test for an action of ON_COOLDOWN.

--- app/reducers/skill-reducer.js
+++ app/reducers/skill-reducer.js
@@ -1,6 +1,9 @@
 'use strict';
 
+import {classSkills} from '../data/class-skills.js';
+
 export default function reducer(state={}, action) {
+  if (action.type === 'SET_SKILLS') return {...classSkills[action.payload]};
   if (action.type === 'ON_COOLDOWN') {
     let newState = {...state};
     newState[action.payload].onCooldown = true;
--- test/reducers/skills-reducer.test.js
+++ test/reducers/skills-reducer.test.js
@@ -6,4 +6,12 @@ describe('skills reducer', () => {
   it('should have an initialState of an empty object', () => {
     expect(reducer(undefined, {})).toEqual({});
   });
+
+  describe('With an action of "ON_COOLDOWN"', () => {
+    it('should set the passed in skill onCooldown to true', () => {
+      expect(reducer({slash: {onCooldown: false}}, {type: 'ON_COOLDOWN', payload: 'slash'})).toEqual({
+        slash: {onCooldown: true}
+      });
+    });
+  });
 });
