e07a16f2183e213fdaa57e12ed0e900254e18425
Steven Bateman <batemanvo@gmail.com>
2017-07-18 17:24:23 -0700

    Added a healing skill to cleric skills and added more properties to skills.

---
+++ app/data/skills/cleric-skills.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export const clericSkills = {
+  heal: {
+    skillName: 'heal',
+    onCooldown: false,
+    atkMultiplier: 0,
+    matMultiplier: 1.5,
+    addHealing: 0,
+    addDamage: 0,
+    playerEcho: ' emit a soothing blue aura, restoring  ',
+    roomEcho: ' emits a soothing blue aura, restoring ',
+    postMessage: ' health to ',
+    skillTypes: ['healing', 'magical']
+  }
+};
--- app/data/skills/warrior-skills.js
+++ app/data/skills/warrior-skills.js
@@ -5,8 +5,12 @@ export const warriorSkills = {
     skillName: 'slash',
     onCooldown: false,
     atkMultiplier: 1.5,
+    matMultiplier: 0,
+    addHealing: 0,
     addDamage: 0,
     playerEcho: ' quickly slash at your opponent, dealing ',
-    roomEcho: ' quickly slashes at their opponent, dealing '
+    roomEcho: ' quickly slashes at their opponent, dealing ',
+    postMessage: ' damage to ',
+    skillTypes: ['damage', 'physical']
   }
 };
