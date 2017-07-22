cc58ae9f8ed27618ec7cc00dc11932db76bb339b
Steven Bateman <batemanvo@gmail.com>
2017-07-22 14:18:07 -0700

    Removed drinkPotion and damageUser actions and changed them to a changeStat user action.

--- app/actions/item-actions.js
+++ app/actions/item-actions.js
@@ -1,15 +1,5 @@
 'use strict';
 
-export function restoreStat(item) {
-  return {
-    type: 'DRINK_POTION',
-    payload: {
-      statToChange: item.statToChange,
-      amount: item.amount
-    }
-  };
-}
-
 export function wearEquipment(eq) {
   return {
     type: 'WEAR_EQUIPMENT',
