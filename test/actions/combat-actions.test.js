13fc98a12676387328c8cc94eab54fbac052f845
Steven Bateman <batemanvo@gmail.com>
2017-07-22 14:22:10 -0700

    Updated combat, item, and user actions tests for new changeStat functionality.

--- test/actions/combat-actions.test.js
+++ test/actions/combat-actions.test.js
@@ -1,17 +1,11 @@
 'use strict';
 
-import {enterCombat, damageUser, slayEnemy, escapeCombat, addEffect, removeEffect, fullRestore} from '../../app/actions/combat-actions.js';
+import {enterCombat, slayEnemy, escapeCombat, addEffect, removeEffect, fullRestore} from '../../app/actions/combat-actions.js';
 
 describe('combat actions', () => {
   describe('enterCombat', () => {
     it('should return a type of ENTER_COMBAT and a payload of what got passed in', () => {
       expect(enterCombat('ayy')).toEqual({type: 'ENTER_COMBAT', payload: 'ayy'});
-    });
-  });
-
-  describe('damageUser', () => {
-    it('should return a type of DAMAGE_USER and a payload of what got passed in', () => {
-      expect(damageUser({damage: 2})).toEqual({type: 'DAMAGE_USER', payload: 2});
     });
   });
 
--- test/actions/user-actions.test.js
+++ test/actions/user-actions.test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-import {truncateDescription, addDescriptionParagraph, clearDescription, tickRegen} from '../../app/actions/user-actions.js';
+import {truncateDescription, addDescriptionParagraph, clearDescription, tickRegen, changeStat} from '../../app/actions/user-actions.js';
 
 describe('User actions', () => {
   describe('truncateDescription', () => {
@@ -24,6 +24,12 @@ describe('User actions', () => {
   describe('tickRegen', () => {
     it('should return an object with a type of "TICK_REGEN"', () => {
       expect(tickRegen()).toEqual({type: 'TICK_REGEN'});
+    });
+  });
+
+  describe('changeStat', () => {
+    it('should return an object with a type of "CHANGE_STAT" and a payload object with statToChange and amount', () => {
+      expect(changeStat({statToChange: 'hp', amount: 4})).toEqual({type: 'CHANGE_STAT', payload: {statToChange: 'hp', amount: 4}});
     });
   });
 });
