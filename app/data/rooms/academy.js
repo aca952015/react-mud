e083783455d8bb25a25cd782b463099aea71ffcc
Steven Bateman <batemanvo@gmail.com>
2017-07-24 20:47:52 -0700

    Added a basic academy entrance.

---
+++ app/data/rooms/academy.js
@@ -0,0 +1,17 @@
+'use strict';
+
+import newItem from '../items.js';
+import newMob from '../mobs.js';
+
+export const Academy = {
+  'Academy Entrance': {
+    roomName: 'Academy Entrance',
+    desc: 'Welcome to the Academy. Type HELP to get a list of commands available.',
+    exits: {},
+    items: [],
+    examines: [],
+    mobs: [],
+    itemResetTimer: 0,
+    mobResetTimer: 0
+  },
+};
