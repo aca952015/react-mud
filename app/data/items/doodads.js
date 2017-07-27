4a65bab1e541ac68bdf113149b996963835bd1d3
Steven Bateman <batemanvo@gmail.com>
2017-07-27 08:53:37 -0700

    Added a 'doodad' item category and created a sign.

---
+++ app/data/items/doodads.js
@@ -0,0 +1,14 @@
+'use strict';
+
+export const doodads = {
+  'academy entrance sign': {
+    name: 'academy entrance sign',
+    short: 'a simple wooden sign',
+    type: 'doodad',
+    long: 'A sign hangs on the wall here, with a simple phrase written on it. (LOOK SIGN to read it)',
+    terms: ['sign'],
+    category: 'doodads',
+    drink: null,
+    description: 'Type HELP for a list of help topics about interacting with the world of Tempest.'
+  },
+};
