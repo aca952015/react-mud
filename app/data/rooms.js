92ad52941e7453e284c92e6dc186346f56c329a9
Steven Bateman <batemanvo@gmail.com>
2017-07-27 09:12:34 -0700

    Added academy to roomData.

--- app/data/rooms.js
+++ app/data/rooms.js
@@ -1,9 +1,11 @@
 'use strict';
 
 import {testAreas} from './rooms/test-areas.js';
+import {academy} from './rooms/academy.js';
 
 export const roomData = {
   ...testAreas,
+  ...academy,
   'Login Room': {
     roomName: 'Welcome to Tempest',
     desc: 'Welcome to Tempest. To make a new character, type "new". To login to an existing character, enter the character\'s name.',
--- app/data/rooms/academy.js
+++ app/data/rooms/academy.js
@@ -3,7 +3,7 @@
 import newItem from '../items.js';
 import newMob from '../mobs.js';
 
-export const Academy = {
+export const academy = {
   'Academy Entrance': {
     roomName: 'Academy Entrance',
     desc: 'Simple wooden columns flank the entrance into Tempest\'s training academy, stretching up to a high ceiling. A pair of crossed swords sits above a fireplace, resting off to one side, a quietly crackling flame warming the walkway as it approaches the foyer to the North. To the South, a large, oak-framed doorway leads back out into the rainy cobblestone streets.',
