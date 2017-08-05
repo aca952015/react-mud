5c36ad377bfdbcfdd2e1f7a60b56eca481467b8e
Steven Bateman <batemanvo@gmail.com>
2017-08-05 15:02:43 -0700

    Added duration to effects component.

--- app/components/effects.jsx
+++ app/components/effects.jsx
@@ -5,15 +5,19 @@ import PropTypes from 'prop-types';
 
 export const Effects = props => {
   const effectBlocks = Object.keys(props.effects).map((effectName, i) => {
-    const allEffects = Object.keys(props.effects[effectName]).map((effectStat, j) => {
-      return <li key={j}>
-        <span>{`${effectStat[0].toUpperCase()}${effectStat.slice(1)}`}: </span>
-        <span>{props.effects[effectName][effectStat] > 0 ? '+' : '-'}{props.effects[effectName][effectStat]}</span>
-      </li>;
-    });
+    const allEffects = Object.keys(props.effects[effectName]).reduce((acc, effectStat, j) => {
+      if (effectStat !== 'duration') {
+        acc.push(<li key={j}>
+          <span>{`${effectStat[0].toUpperCase()}${effectStat.slice(1)}`}: </span>
+          <span>{props.effects[effectName][effectStat] > 0 ? '+' : '-'}{props.effects[effectName][effectStat]}</span>
+        </li>);
+      }
+      return acc;
+    }, []);
 
+    const tickGrammar = props.effects[effectName].duration === 1 ? 'tick' : 'ticks';
     return <div key={i} className="effect-block">
-      <p>{`${effectName[0].toUpperCase()}${effectName.slice(1)}`}</p>
+      <p>{`${effectName[0].toUpperCase()}${effectName.slice(1)}`} ({props.effects[effectName].duration} {tickGrammar})</p>
       <ul>{allEffects}</ul>
       <div className="clearfix" style={{'clear': 'both'}}></div>
     </div>;
