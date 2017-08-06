7c3e25e7670deae1341a7c8e6ed63ae08e462dd4
Steven Bateman <batemanvo@gmail.com>
2017-08-06 14:51:34 -0700

    Updated the effects component to only show duration if there is a duration.

--- app/components/effects.jsx
+++ app/components/effects.jsx
@@ -17,7 +17,7 @@ export const Effects = props => {
 
     const tickGrammar = props.effects[effectName].duration === 1 ? 'tick' : 'ticks';
     return <div key={i} className="effect-block">
-      <p>{`${effectName[0].toUpperCase()}${effectName.slice(1)}`} ({props.effects[effectName].duration} {tickGrammar})</p>
+      <p>{`${effectName[0].toUpperCase()}${effectName.slice(1)}`} {props.effects[effectName].duration ? `(${props.effects[effectName].duration} ${tickGrammar})` : null }</p>
       <ul>{allEffects}</ul>
       <div className="clearfix" style={{'clear': 'both'}}></div>
     </div>;
