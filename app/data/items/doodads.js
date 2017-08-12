45667c596e4c1e5a3e576977a6853b53c23402d7
Steven Bateman <batemanvo@gmail.com>
2017-08-12 08:06:13 -0700

    Added a combat training hall directions sign.

--- app/data/items/doodads.js
+++ app/data/items/doodads.js
@@ -11,4 +11,14 @@ export const doodads = {
     drink: null,
     description: 'Type HELP for a list of help topics about interacting with the world of Tempest.'
   },
+  'combat training directions': {
+    name: 'combat training directions',
+    short: 'a simple wooden sign',
+    type: 'doodad',
+    long: 'A sign hangs on the wall here, with some directions scrawled across it.',
+    terms: ['sign', 'directions'],
+    category: 'doodads',
+    drink: null,
+    description: 'WEST is where you can learn about basic combat interaction. NORTH is where you can learn about skills.'
+  }
 };
