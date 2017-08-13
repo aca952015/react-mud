7ad573c0631f13bac263d815fb1759fc19e8869e
Steven Bateman <batemanvo@gmail.com>
2017-08-13 16:06:44 -0700

    Created an animated dummy mob for the academy.

---
+++ app/data/mobs/academy-mobs.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export const academyMobs = {
+  'animated dummy': {
+    name: 'animated dummy',
+    description: 'A collection of old, worn-down armor and a blunted sword held together in the shape of a human, this dummy is constructed from a force of magic binding it all into place.',
+    short: 'an animated dummy',
+    long: 'An animated dummy, made up of bits of old armor bound together by magic, floats around here.',
+    terms: ['dummy', 'animated'],
+    hp: 10,
+    maxHP: 10,
+    atk: 1,
+    def: 0,
+    mdf: 0
+  }
+};
