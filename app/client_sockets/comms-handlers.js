e8e9d75a6bcc67e866564fab765bff4be98312f5
Steven Bateman <batemanvo@gmail.com>
2017-08-19 12:03:57 -0700

    Constantized variables in comms-handlers.

--- app/client_sockets/comms-handlers.js
+++ app/client_sockets/comms-handlers.js
@@ -4,8 +4,9 @@ import {newMessage} from '../actions/message-actions.js';
 import whisperProcessor from '../processors/whisper-processor.js';
 
 export default function commsHandlers(homeCtx) {
-  let socket = homeCtx.socket;
-  let props = homeCtx.props;
+  const socket = homeCtx.socket;
+  const props = homeCtx.props;
+  
   socket.on('generalMessage', result => props.dispatch(newMessage(result)));
   socket.on('whisperSuccess', result => props.dispatch(newMessage(whisperProcessor(result, homeCtx.props.username))));
   socket.on('whisperFail', () => props.dispatch(newMessage({feedback: 'I don\'t see that person here.'})));
