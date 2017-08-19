a1b2342d8539c32316b2a8495b00aadfae4d1fb1
Steven Bateman <batemanvo@gmail.com>
2017-08-19 13:10:54 -0700

    Constantized a variable in examine-handler.

--- app/handlers/examine-handler.js
+++ app/handlers/examine-handler.js
@@ -6,7 +6,7 @@ import termsProcessor from '../processors/terms-processor.js';
 export default function examineHandler(command, args, props) {
   if (!args) return {funcsToCall: [newMessage], feedback: 'Examine what?'};
   args = args.toLowerCase();
-  let foundItem = termsProcessor(props.inventory, args.split('.'));
+  const foundItem = termsProcessor(props.inventory, args.split('.'));
 
   if (!foundItem) return {funcsToCall: [newMessage], feedback: 'You aren\'t carrying that.'};
   return {funcsToCall: [newMessage], feedback: foundItem.description};
