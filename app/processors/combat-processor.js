41902e241a775d9d57ba53c3fe484e71109bf7f4
Steven Bateman <batemanvo@gmail.com>
2017-08-19 13:25:14 -0700

    Constantized variables in combat-processor.

--- app/processors/combat-processor.js
+++ app/processors/combat-processor.js
@@ -4,8 +4,8 @@ import equipmentStatProcessor from './equipment-stat-processor.js';
 
 export default function combatProcessor(socket, props) {
   // Pick a random enemy among the targets currently being fought and damage them.
-  let rand = Math.floor(Math.random() * props.combat.targets.length);
-  let enemy = props.combat.targets[rand];
+  const rand = Math.floor(Math.random() * props.combat.targets.length);
+  const enemy = props.combat.targets[rand];
 
   // Add up the atk bonuses from all equipped items, the user's atk, and the enemy's def
   // The final result is the amount of damage to deal. Damage cannot be less than 1.
