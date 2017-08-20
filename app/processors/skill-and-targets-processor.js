e870d36f09053827dad13d569ba617b029a4b9a8
Steven Bateman <batemanvo@gmail.com>
2017-08-20 16:25:03 -0700

    Constantized variables in skill-and-targets processor.

--- app/processors/skill-and-targets-processor.js
+++ app/processors/skill-and-targets-processor.js
@@ -8,17 +8,17 @@ export default function skillAndTargetsProcessor(command, args, props) {
   // "searing bat" should also return "searing light" as a skill and "bat" as args.
   // "searing light" should return "searing light" as a skill and "undefined" as args.
   // "slash bat" should return "slash" as a skill and "bat" as args.
-  // "slash" should return "slash" as a skill and "bat" as args.
+  // "slash" should return "slash" as a skill and "undefined" as args.
   if (args) {
-    let regEx = new RegExp(`^${command} ${args.split(' ')[0]}`, 'i');
-    let targetedSkill = skills.find(skill => skill.match(regEx));
+    const regEx = new RegExp(`^${command} ${args.split(' ')[0]}`, 'i');
+    const targetedSkill = skills.find(skill => skill.match(regEx));
     if (targetedSkill) {
       if (args.split(' ').length > 1) return {targetedSkill, args: args.split(' ')[1]};
       return {targetedSkill, args: undefined};
     }
   }
 
-  let regEx = new RegExp(`^${command}`, 'i');
-  let targetedSkill = skills.find(skill => skill.match(regEx));
+  const regEx = new RegExp(`^${command}`, 'i');
+  const targetedSkill = skills.find(skill => skill.match(regEx));
   return {targetedSkill, args};
 }
