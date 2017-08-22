20dd75fe3439e14864b31cad0274690ee8a4d047
Steven Bateman <batemanvo@gmail.com>
2017-08-21 18:41:55 -0700

    Constantized variables in slay-enemy.

--- app/reducers/lib/user_reducer/slay-enemy.js
+++ app/reducers/lib/user_reducer/slay-enemy.js
@@ -3,10 +3,10 @@
 export default function slayEnemy(state, action) {
   // Remove the enemy from the current combat array. If they are the last enemy,
   // remove the user from combat.
-  let slainEnemy = state.combat.targets.find(enemy => enemy.id === action.payload.id);
-  let prevTargets = state.combat.targets.slice(0, state.combat.targets.indexOf(slainEnemy));
-  let endTargets = state.combat.targets.slice(state.combat.targets.indexOf(slainEnemy) + 1);
-  let newTargets = prevTargets.concat(endTargets);
+  const slainEnemy = state.combat.targets.find(enemy => enemy.id === action.payload.id);
+  const prevTargets = state.combat.targets.slice(0, state.combat.targets.indexOf(slainEnemy));
+  const endTargets = state.combat.targets.slice(state.combat.targets.indexOf(slainEnemy) + 1);
+  const newTargets = prevTargets.concat(endTargets);
   if (!newTargets.length) return {...state, combat: {targets: [], active: false}};
   return {...state, combat: {targets: newTargets, active: true}};
 }
