ab7e85510f2ab26f03424a76eb41393b906f5cf5
Steven Bateman <batemanvo@gmail.com>
2017-08-21 18:36:58 -0700

    Constantized and spaced out change-stat.

--- app/reducers/lib/user_reducer/change-stat.js
+++ app/reducers/lib/user_reducer/change-stat.js
@@ -2,10 +2,13 @@
 
 export default function changeStat(state, action) {
   let currentStat = state[action.payload.statToChange] - action.payload.amount;
-  let maxStat = state[`max${action.payload.statToChange.toUpperCase()}`];
+  const maxStat = state[`max${action.payload.statToChange.toUpperCase()}`];
+
   if (currentStat > maxStat) currentStat = maxStat;
   if (currentStat < 0) currentStat = 0;
-  let newState = {...state};
+
+  const newState = {...state};
   newState[action.payload.statToChange] = currentStat;
+
   return newState;
 }
