59a003d756d8e0da3bd7db14e60c7246f7b89f39
Steven Bateman <batemanvo@gmail.com>
2017-08-21 18:41:01 -0700

    Constantized variables, removed unnecessary assignments.

--- app/reducers/lib/user_reducer/drop-item.js
+++ app/reducers/lib/user_reducer/drop-item.js
@@ -4,6 +4,5 @@ export default function dropItem(state, action) {
   const itemToDrop = state.inventory.find(item => item.id === action.payload.id);
   const prevItems = state.inventory.slice(0, state.inventory.indexOf(itemToDrop));
   const endItems = state.inventory.slice(state.inventory.indexOf(itemToDrop) + 1);
-  const newInventory = prevItems.concat(endItems);
-  return {...state, inventory: newInventory};
+  return {...state, inventory: prevItems.concat(endItems)};
 }
