3a70dfc4963da0d4583654caae19ae7442f5173f
Steven Bateman <batemanvo@gmail.com>
2017-08-21 18:37:21 -0700

    Constantized drop-item.

--- app/reducers/lib/user_reducer/drop-item.js
+++ app/reducers/lib/user_reducer/drop-item.js
@@ -1,9 +1,9 @@
 'use strict';
 
 export default function dropItem(state, action) {
-  let itemToDrop = state.inventory.find(item => item.id === action.payload.id);
-  let prevItems = state.inventory.slice(0, state.inventory.indexOf(itemToDrop));
-  let endItems = state.inventory.slice(state.inventory.indexOf(itemToDrop) + 1);
-  let newInventory = prevItems.concat(endItems);
+  const itemToDrop = state.inventory.find(item => item.id === action.payload.id);
+  const prevItems = state.inventory.slice(0, state.inventory.indexOf(itemToDrop));
+  const endItems = state.inventory.slice(state.inventory.indexOf(itemToDrop) + 1);
+  const newInventory = prevItems.concat(endItems);
   return {...state, inventory: newInventory};
 }
