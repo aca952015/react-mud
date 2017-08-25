a39bea48dcb4b70fc06d0dfe3307e01b98b3d34e
Steven Bateman <batemanvo@gmail.com>
2017-08-25 15:43:07 -0700

    Added a basic damage skill to a new mobSkills object.

---
+++ app/data/mob-skills.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import {academyMobSkills} from './skills/academy-mob-skills.js';
+
+export const mobSkills = {
+  ...academyMobSkills
+};
---
+++ app/data/skills/academy-mob-skills.js
@@ -0,0 +1,16 @@
+'use strict';
+
+export const academyMobSkills = {
+  'bash': {
+    skillName: 'bash',
+    cooldownTime: 2,
+    cooldownRemaining: 1,
+    atkMultiplier: 1,
+    matMultiplier: 0,
+    addHealing: 0,
+    addDamage: 0,
+    roomEcho: ' lets loose a heavy bash, dealing ',
+    postMessage: ' damage to ',
+    skillTypes: ['damage', 'physical']
+  },
+};
