6ab757aadb2d2499d0cd57193c2e0b6e3eb22171
Steven Bateman <batemanvo@gmail.com>
2017-08-27 13:34:29 -0700

    Added a container key.

--- app/data/items/keys.js
+++ app/data/items/keys.js
@@ -1,6 +1,26 @@
 'use strict';
 
 export const keys = {
+  'academy key': {
+    name: 'academy key',
+    short: 'the academy key',
+    type: 'items',
+    long: 'A simple black key lies here.',
+    terms: ['simple', 'black', 'key'],
+    category: 'keys',
+    drink: null,
+    description: 'This key has a simple carving of a book on the end of it, with a few intricately cut teeth at its tip.'
+  },
+  'container key': {
+    name: 'container key',
+    short: 'the equipment room key',
+    type: 'items',
+    long: 'A silver key lies here.',
+    terms: ['silver', 'key'],
+    category: 'keys',
+    drink: null,
+    description: 'This bare key is made of silver, with only a single tooth at its end.'
+  },
   'gallows key': {
     name: 'gallows key',
     short: 'a small black key',
@@ -10,16 +30,6 @@ export const keys = {
     category: 'keys',
     drink: null,
     description: 'This is a placeholder description for a gallows key.'
-  },
-  'academy key': {
-    name: 'academy key',
-    short: 'the academy key',
-    type: 'items',
-    long: 'A key for unlocking the get from container room lies here.',
-    terms: ['key'],
-    category: 'keys',
-    drink: null,
-    description: 'This is a placeholder description for the academy key.'
   },
   'secret key': {
     name: 'secret key',
@@ -31,16 +41,6 @@ export const keys = {
     drink: null,
     description: 'This is a secret key! You should not be holding it.'
   },
-  'useless key': {
-    name: 'useless key',
-    short: 'a useless key',
-    type: 'items',
-    long: 'A useless key whose teeth have been completely worn off lies here.',
-    terms: ['useless', 'key'],
-    category: 'keys',
-    drink: null,
-    description: 'This is a placeholder description for a useless key.'
-  },
   'tester key': {
     name: 'tester key',
     short: 'a tester\'s key',
@@ -50,5 +50,15 @@ export const keys = {
     category: 'keys',
     drink: null,
     description: 'This is a placeholder description for a tester key.'
+  },
+  'useless key': {
+    name: 'useless key',
+    short: 'a useless key',
+    type: 'items',
+    long: 'A useless key whose teeth have been completely worn off lies here.',
+    terms: ['useless', 'key'],
+    category: 'keys',
+    drink: null,
+    description: 'This is a placeholder description for a useless key.'
   }
 };
