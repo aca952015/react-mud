a7ef0a34b75660cfcfad484caf97274f6e0cb301
Steven Bateman <batemanvo@gmail.com>
2017-09-04 14:22:08 -0700

    Refactored itemRespawnProcessor test to pass current tests. Also removed mob processor tests, since they are no longer applicable to that file.

--- test/processors/item-respawn-processor.test.js
+++ test/processors/item-respawn-processor.test.js
@@ -2,83 +2,59 @@
 
 import itemRespawnProcessor from '../../app/processors/item-respawn-processor.js';
 import newItem from '../../app/data/items.js';
-import newMob from '../../app/data/mobs.js';
 
 describe('item respawn processor', () => {
   let originalRoom = [newItem('equipment', 'leather helm'), newItem('equipment', 'leather helm'), newItem('keys', 'gallows key')];
   let currentRoom = [newItem('keys', 'gallows key'), newItem('equipment', 'leather helm'), newItem('equipment', 'leather helm')];
-  let originalMobs = [newMob('bat'), newMob('bat'), newMob('armored zombie')];
-  let currentMobs = [newMob('bat'), newMob('bat'), newMob('armored zombie')];
 
   describe('With a room that isn\'t missing any items', () => {
     it('should return an empty array', () => {
-      expect(itemRespawnProcessor(originalRoom, currentRoom)).toEqual([]);
+      expect(itemRespawnProcessor(originalRoom, currentRoom)).toEqual({itemsToRespawn: [], itemsToRemove: []});
     });
   });
 
   describe('With a room that has extra items', () => {
     it('should return an empty array', () => {
-      expect(itemRespawnProcessor(originalRoom, [...currentRoom, newItem('potions', 'health potion')])).toEqual([]);
+      expect(itemRespawnProcessor(originalRoom, [...currentRoom, newItem('potions', 'health potion')])).toEqual({
+        itemsToRespawn: [],
+        itemsToRemove: []
+      });
     });
   });
 
   describe('With a room missing all of an original item', () => {
     it('should respawn the correct number of items', () => {
-      expect(itemRespawnProcessor(originalRoom, [newItem('keys', 'gallows key')])).toEqual([
-        {
-          category: 'equipment',
-          name: 'leather helm'
-        },
-        {
-          category: 'equipment',
-          name: 'leather helm'
-        }
-      ]);
+      expect(itemRespawnProcessor(originalRoom, [newItem('keys', 'gallows key')])).toEqual({
+        itemsToRespawn: [
+          {
+            category: 'equipment',
+            name: 'leather helm'
+          },
+          {
+            category: 'equipment',
+            name: 'leather helm'
+          }
+        ],
+        itemsToRemove: []
+      });
     });
   });
 
   describe('With a room missing only some of the original items', () => {
     it('should respawn the correct number of items', () => {
-      expect(itemRespawnProcessor(originalRoom, [newItem('equipment', 'leather helm')])).toEqual([
-        {
-          category: 'equipment',
-          name: 'leather helm'
-        },
-        {
-          category: 'keys',
-          name: 'gallows key'
-        }
-      ]);
-    });
-  });
-
-  describe('With a room not missing any mobs', () => {
-    it('should return an empty array', () => {
-      expect(itemRespawnProcessor(originalMobs, currentMobs)).toEqual([]);
-    });
-  });
-
-  describe('With a room that has extra mobs', () => {
-    it('should return an empty array', () => {
-      expect(itemRespawnProcessor(originalMobs, [...currentMobs, newMob('bat')])).toEqual([]);
-    });
-  });
-
-  describe('With a room missing all of one type of mob', () => {
-    it('should respawn the correct number of items', () => {
-      expect(itemRespawnProcessor(originalMobs, [newMob('armored zombie')])).toEqual([
-        {name: 'bat'},
-        {name: 'bat'}
-      ]);
-    });
-  });
-
-  describe('With a room missing only some of a type of a mob', () => {
-    it('should respawn the correct number of mobs', () => {
-      expect(itemRespawnProcessor(originalMobs, [newMob('bat')])).toEqual([
-        {name: 'bat'},
-        {name: 'armored zombie'}
-      ]);
+      expect(itemRespawnProcessor(originalRoom, [newItem('equipment', 'leather helm')])).toEqual({
+        itemsToRespawn: [
+          {
+            category: 'equipment',
+            name: 'leather helm'
+          },
+          {
+            category: 'keys',
+            name: 'gallows key'
+          }
+        ],
+        itemsToRemove: []
+      });
     });
   });
 });
