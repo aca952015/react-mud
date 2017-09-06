5f1a5cbe90b34b23a729391e6f4062a7925a1da3
Steven Bateman <batemanvo@gmail.com>
2017-09-05 20:52:23 -0700

    Added a test for respawning 3 containers with the same kind of item.

--- test/processors/item-respawn-processor.test.js
+++ test/processors/item-respawn-processor.test.js
@@ -4,8 +4,29 @@ import itemRespawnProcessor from '../../app/processors/item-respawn-processor.js
 import newItem from '../../app/data/items.js';
 
 describe('item respawn processor', () => {
-  let originalRoom = [newItem('equipment', 'leather helm'), newItem('equipment', 'leather helm'), newItem('keys', 'gallows key')];
-  let currentRoom = [newItem('keys', 'gallows key'), newItem('equipment', 'leather helm'), newItem('equipment', 'leather helm')];
+  const originalRoom = [newItem('equipment', 'leather helm'), newItem('equipment', 'leather helm'), newItem('keys', 'gallows key')];
+  const currentRoom = [newItem('keys', 'gallows key'), newItem('equipment', 'leather helm'), newItem('equipment', 'leather helm')];
+  const backpackWithPotion = {
+    ...newItem('containers', 'backpack'),
+    container: {
+      holds: ['items', 'weapons', 'equipment'],
+      contains: [newItem('potions', 'health potion')]
+    }
+  };
+  const backpackWithKey = {
+    ...newItem('containers', 'backpack'),
+    container: {
+      holds: ['items', 'weapons', 'equipment'],
+      contains: [newItem('keys', 'academy key')]
+    }
+  };
+  const backpackWithSword = {
+    ...newItem('containers', 'backpack'),
+    container: {
+      holds: ['items', 'weapons', 'equipment'],
+      contains: [newItem('weapons', 'broad sword')]
+    }
+  };
 
   describe('With a room that isn\'t missing any items', () => {
     it('should return an empty array', () => {
@@ -23,19 +44,48 @@ describe('item respawn processor', () => {
   });
 
   describe('With a room missing all of an original item', () => {
-    it('should respawn the correct number of items', () => {
-      expect(itemRespawnProcessor(originalRoom, [newItem('keys', 'gallows key')])).toEqual({
-        itemsToRespawn: [
-          {
-            category: 'equipment',
-            name: 'leather helm'
-          },
-          {
-            category: 'equipment',
-            name: 'leather helm'
-          }
-        ],
-        itemsToRemove: []
+    describe('That aren\'t containers', () => {
+      it('should respawn the correct number of items', () => {
+        expect(itemRespawnProcessor(originalRoom, [newItem('keys', 'gallows key')])).toEqual({
+          itemsToRespawn: [
+            {
+              category: 'equipment',
+              name: 'leather helm'
+            },
+            {
+              category: 'equipment',
+              name: 'leather helm'
+            }
+          ],
+          itemsToRemove: []
+        });
+      });
+    });
+
+    describe('With containers', () => {
+      describe('With three containing the same item', () => {
+        const roomItems = [backpackWithPotion, backpackWithPotion, backpackWithPotion];
+        const currentItems = [];
+        expect(itemRespawnProcessor(roomItems, currentItems)).toEqual({
+          itemsToRespawn: [
+            {
+              category: backpackWithPotion.category,
+              respawnContents: [backpackWithPotion.container.contains[0]],
+              name: backpackWithPotion.name
+            },
+            {
+              category: backpackWithPotion.category,
+              respawnContents: [backpackWithPotion.container.contains[0]],
+              name: backpackWithPotion.name
+            },
+            {
+              category: backpackWithPotion.category,
+              respawnContents: [backpackWithPotion.container.contains[0]],
+              name: backpackWithPotion.name
+            }
+          ],
+          itemsToRemove: []
+        });
       });
     });
   });
