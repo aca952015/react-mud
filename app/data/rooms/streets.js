57074d7466437cb666b824faefd6c88638875769
Steven Bateman <batemanvo@gmail.com>
2017-09-11 20:21:49 -0700

    Started construction of the Tempest Streets section.

---
+++ app/data/rooms/streets.js
@@ -0,0 +1,21 @@
+'use strict';
+
+export const streets = {
+  'Streets - Academy Entrance': {
+    roomName: 'Streets - Academy Entrance',
+    roomTitle: 'Before the Academy',
+    desc: 'A sharply peaked eve rests over the entrance to a stone building to the North, stretching down to just above street level. Heavy rain cascades down either side, before running to a grated drain near the intersection of streets. Some of the cobblestone walkway has been worn away, holes revealing deeply muddied patches of earth. To the South, the street stretches towards the town center.',
+    exits: {
+      north: {
+        exit: 'Academy - Academy Entrance',
+        locked: false
+      },
+    },
+    items: [],
+    examines: [],
+    mobs: [],
+    itemResetTimer: 0,
+    mobResetTimer: 0,
+    lockedExitTimer: 0
+  },
+};
